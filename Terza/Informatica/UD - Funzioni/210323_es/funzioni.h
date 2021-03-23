dd89dcf15227fd20094a70b654887cb275999cf6
Francesco Pradella <50577204+CescoP81@users.noreply.github.com>
2021-03-23 11:51:29 +0100

    Terza, esercizio della mattina.

---
+++ Terza/Informatica/UD - Funzioni/210323_es/funzioni.h
@@ -0,0 +1,35 @@
+/* calcolo della media del vettore 
+@param int[] vettore da utilizzare
+@param int dimensione del vettore
+@return int valore della media */
+int mediaVettore(int v[], int dimensione);
+
+/* stampa valori inferiori o uguali alla media */
+void valoriSottoMedia(int v[], int dimensione);
+
+/* Determinazione del valore maggiore
+@param int[] vettore da utilizzare 
+@param int dimensione del vettore
+@return int valore maggiore */
+int maggioreVettore(int v[], int dimensione);
+
+/* Scostamenti dal valore maggiore
+@param int[] valore da utilizzare
+@param int dimensione del vettore
+@return void*/
+void scostamentiVettore(int v[], int dimensione);
+
+/* Ordinamento del vettore, crescente o decrescente selezionato come parametro
+@param int[] valore da utilizzare
+@param int dimensione del vettore
+@param int modo di ordinamento 
+@return int flag di controllo su esecuzione positiva*/
+int ordinaVettore(int v[], int dimensione, int modo);
+
+
+
+
+
+
+
+
