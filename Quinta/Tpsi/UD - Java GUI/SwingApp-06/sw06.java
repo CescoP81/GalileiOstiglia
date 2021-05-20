d3ef7b9180e8cdc3f0a945668404174b58188f14
Francesco Pradella <50577204+CescoP81@users.noreply.github.com>
2021-05-20 10:29:50 +0200

    Aggiornamento.

--- Quinta/Tpsi/UD - Java GUI/SwingApp-06/sw06.java
+++ Quinta/Tpsi/UD - Java GUI/SwingApp-06/sw06.java
@@ -4,10 +4,10 @@ import javax.swing.*;
 
 
 public class sw06 extends JFrame{
-   JFrame f;
-   JPanel p0;
-   JPanel p1;
-   JPanel p2;
+   JFrame f;   // finestra principale
+   JPanel p0;  // Prevede il layout a righe consecutive.
+   JPanel p1;  // Comprende i componenti della prima riga.
+   JPanel p2;  // Comprende la label da modificare con l'output del programma.
    JButton btn;
    JTextField tf;
    JLabel sStringa, sRisultati;
@@ -15,14 +15,14 @@ public class sw06 extends JFrame{
    public sw06(){
       f = new JFrame();
       p0 = new JPanel();
-      p0.setLayout(new GridLayout(3,1));
+      p0.setLayout(new GridLayout(2,1));
 
       p1 = new JPanel();
       p1.setLayout(new FlowLayout(FlowLayout.LEFT));
-      sStringa = new JLabel("Stringa: ");
+      sStringa = new JLabel("Numero: ");
       tf = new JTextField();
       tf.setPreferredSize(new Dimension(100,20));
-      btn = new JButton("Verifica");
+      btn = new JButton("Verifica Primo");
       btn.addActionListener(new btnListener());
       p1.add(sStringa);
       p1.add(tf);
@@ -37,30 +37,42 @@ public class sw06 extends JFrame{
       
 
       setLayout(new BoxLayout(p0,BoxLayout.Y_AXIS));
-      p0.add(p1);
-      p0.add(p2);
+      p0.add(p1); // aggiungo il pannello della prima riga.
+      p0.add(p2); // aggiungere il pannello della seconda riga.
       p0.setAlignmentY(Component.TOP_ALIGNMENT);
      
       f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       f.add(p0);
       f.setSize(400,300);
       f.setTitle("Swing App 06");
-      //f.pack();
+      f.pack();
       f.setVisible(true);
-
    }
 
    private class btnListener implements ActionListener{
       @Override
       public void actionPerformed(ActionEvent ae){
          String str;
+         String sNum = tf.getText(); // recupero il numero nella textfield come stringa.
+         int num = Integer.parseInt(sNum); // converto la stringa in intero.
+         int cnt;
+
          str = "<html>";
-         str += sRisultati.getText();
+         str += "Risultati:";
          str += "<br>";
-         for(int i=1; i<=5; i++){
-            str += i + " ";
+         str += "<br>Verifica se il numero " + sNum + " sia primo:<br>";
+         cnt = 0;
+         for(int i=1; i<=num; i++){
+            if(num%i == 0){
+               str += i + " ";
+               cnt ++;
+            }
          }
          str += "<br>";
+         if(cnt <= 2)
+            str += "Numero Primo.";
+         else
+            str += "Numero non Primo.";
          str += "</html>";
          sRisultati.setText(str);
          f.pack();
