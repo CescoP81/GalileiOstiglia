571f6c42cfceb869bea06d7674e77d7b66fbed06
Francesco Pradella <50577204+CescoP81@users.noreply.github.com>
2021-08-03 16:45:54 +0200

    Update

--- 5 Quinta/Informatica/210304_ProvaPdf/verifica.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('../fpdf182/fpdf.php');
-
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World!');
-$pdf->Output();
-?>
--- 5 Quinta/Informatica/ComuneRoccofritto/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// inclusione di un file di libreria con funzioni sviluppate.
-require("lib.php");
-
-    require("../head.html");
-        writeMainMenu();
-    require("../foot.html");
-?>
