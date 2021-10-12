b114118cd0c03e7afb02827cb4697a30b67f1888
Francesco Pradella <50577204+CescoP81@users.noreply.github.com>
2021-10-12 09:34:54 +0200

    5i update

--- 5 Quinta/Informatica/UD 2 - PHP e mySQL Tables/page1.php
+++ 5 Quinta/Informatica/UD 2 - PHP e mySQL Tables/page1.php
@@ -1,6 +1,10 @@
 <html>
    <head>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+   <!--
+      Riferimento alla guida ufficiale BootStrap versione 5.1 
+      https://getbootstrap.com/docs/5.1/getting-started/introduction/
+   -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
       <title>Tabella di prova con BootStrap</title>
    </head>
    <body>
@@ -42,7 +46,7 @@
    $sql = "SELECT * FROM persone";
    $rs = $db->query($sql);
 
-   echo("<table class=\"table table-dark table-striped table-hover\">
+   echo("<table class=\"table table-striped table-hover\">
       <thead>
          <tr>
             <th scope=\"col\">#</th>
@@ -69,9 +73,7 @@
    $db->close();
 ?>
       </div>
-   <!-- chiusura del body con inserimento richiami BootStrap -->
-   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+   <!-- chiusura del body con inserimento richiami BootStrap Boundle JS -->
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
    </body>
 </html>
