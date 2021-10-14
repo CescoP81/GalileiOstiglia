c70bcdd75095b2964d75dd34c7373d54ad0a130b
Francesco Pradella <50577204+CescoP81@users.noreply.github.com>
2021-10-14 10:08:57 +0200

    up 5i

---
+++ 5 Quinta/Informatica/UD 2 - PHP e mySQL Tables/page2.php
@@ -0,0 +1,9 @@
+<?php
+// richiamo il blocco iniziale HTML per precaricare dati e BootStrap
+require("../head.html");
+
+   echo("Questo messaggio lo vedo spaziato e in carattere diverso dal solito");
+
+   // richiamo il blocco finale con chiusura pagina e caricamento JS di BootStrap
+require("../foot.html");
+?>
