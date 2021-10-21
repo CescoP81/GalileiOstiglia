937a028ccd562fa405a634639801d5c1132efad9
Francesco Pradella <50577204+CescoP81@users.noreply.github.com>
2021-10-21 12:42:38 +0200

    upload 5i

--- 5 Quinta/Informatica/UD 2 - PHP e mySQL Tables/personeTable.php
+++ 5 Quinta/Informatica/UD 2 - PHP e mySQL Tables/personeTable.php
@@ -2,15 +2,10 @@
 require("../funzioni.php");
 require("../head.html");
 
-
 /* Creo la barra di navigazione per la pagina di gestione tabella 'persone'*/
 echo("<ul class=\"nav\">
-  <li class=\"nav-item\">
-    <a class=\"nav-link\" aria-current=\"page\" href=\"personeTable.php\">Visualizza Lista Persone</a>
-  </li>
-  <li class=\"nav-item\">
-    <a class=\"nav-link\" href=\"personeTable.php?scelta=formPersona\">Aggiungi Persona</a>
-  </li>
+  <li class=\"nav-item\"><a class=\"nav-link\" aria-current=\"page\" href=\"personeTable.php\">Visualizza Lista Persone</a></li>
+  <li class=\"nav-item\"><a class=\"nav-link\" href=\"personeTable.php?scelta=formPersona\">Aggiungi Persona</a></li>
 </ul>");
 
    /* scrivo il codice php con la switch-case e relativi spezzoni di codice per le diverse operazioni.*/
@@ -18,18 +13,29 @@ echo("<ul class=\"nav\">
    else $sc = null;
    
    switch($sc){
+      /* Il case formPersona semplicemente visualizza il form con componenti bootstrap per inserire due stringhe,
+      associate al cognome e nome di una persona. Rispetto all'esempio base presente su bootstrap, sono aggiunti
+      gli attributi 'name' nei componenti input, mentre action e method nell'apertura del form.
+      */
       case "formPersona":{
          echo("<form action=\"personeTable.php\" method=\"post\">
-            Cognome:
-            <input class=\"form-control\" type=\"text\" name=\"cognomeP\" placeholder=\"Default input\" aria-label=\"default input example\">
-            Nome:
-            <input class=\"form-control\" type=\"text\" name=\"nomeP\" placeholder=\"Default input\" aria-label=\"default input example\">
+            <label for=\"campo_1\" class=\"form-label\">Cognome:</label>
+            <input class=\"form-control\" type=\"text\" id=\"campo_1\" name=\"cognomeP\" placeholder=\"Default input\" aria-label=\"default input example\">
+            <label for=\"campo_2\" class=\"form-label\">Nome:</label>
+            <input class=\"form-control\" type=\"text\" id=\"campo_2\" name=\"nomeP\" placeholder=\"Default input\" aria-label=\"default input example\">
             <input type=\"hidden\" name=\"scelta\" value=\"addPersona\">
+            <br />
             <button type=\"submit\" class=\"btn btn-primary\">Aggiungi al Database</button>
             </form>");
          break;
       }
 
+      /* Il case addPersona viene utilizzato quando la pagina è richiamata dal FORM di inserimento dati.
+      In quel caso nella barra dell'indirizzo avremo le variabili scelta, nomeP e cognomeP da utilizzare 
+      per la creazione della query SQL di inserimento di un nuovo record. Vedremo in un secondo momento
+      come gestire l'eventualità che l'utente non inserisca il cognome o il nome e quindi avremmo un'anomalia
+      in fase di inserimento dati.
+      */
       case "addPersona":{
          $nome = $_REQUEST['nomeP'];
          $cognome = $_REQUEST['cognomeP'];
@@ -46,6 +52,10 @@ echo("<ul class=\"nav\">
          break;
       }
 
+      /* Il case di default, cioè quando scelta non è definito nell'indirizzo HTTP di chiamata, oppure
+      contiene un valore non valido per lo script, visualizza la tabella 'persone' del database.
+      Possiamo far fare qualunque cosa al default, importante è gestire il caso anomalo in cui 'scelta' non sia definita.
+      */
       default:{
          $db = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB);
          $sql = "SELECT * FROM persone";
