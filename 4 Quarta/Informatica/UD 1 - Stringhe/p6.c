164cca06dc3f0f021c92447654179c28a6fac6f0
Francesco Pradella <50577204+CescoP81@users.noreply.github.com>
2021-10-28 12:39:32 +0200

    upload 4i

---
+++ 4 Quarta/Informatica/UD 1 - Stringhe/p6.c
@@ -0,0 +1,51 @@
+/*
+   Realizzare un programma C che richieda una stringa da tastiera e determina se contiene delle doppie,
+   es tetto, lotto, zappa.
+*/
+#include <conio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void viewString(char *);
+int findDoppie(char *);
+
+int main(){
+   /* creo un puntatore a stringa e successivamente alloco uno spazio per 100 caratteri 99+'\0'.
+      l'area di memoria inizializzata non la riduco in caso la stringa sia più piccola dei
+      100 caratteri massimi previsti.
+   */
+   char *s;
+   s = (char *) malloc(sizeof(char) * 100);
+   
+   // acquisisco la stringa da tastiera.
+   printf("Stringa: ");
+   scanf("%s", s);
+   fflush(stdin);
+
+   // visualizzo la stringa passando il puntatore alla funzione
+   viewString(s);
+
+   // chiamo la funzione per la verifica delle doppie
+   if(findDoppie(s))
+      printf("\nLa parola %s contiene delle doppie.", s);
+   else
+      printf("\nLa parola %s non contiene delle doppie.", s);
+   return(0);
+}
+
+// ---------------------------------
+void viewString(char *_s){
+   while(*_s != '\0'){
+      printf("%c", *_s);
+      _s = _s + 1;
+   }
+}
+
+int findDoppie(char *_s){
+   while(*_s != '\0' && *(_s+1) != '\0'){
+      if(*_s == *(_s+1))
+         return(1);
+      _s = _s + 1;
+   }
+   return(0);
+}
