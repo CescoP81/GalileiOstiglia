e5dd75869212c3dc5387a691d2da77a3c8e48d06
Francesco Pradella <50577204+CescoP81@users.noreply.github.com>
2021-11-04 13:07:03 +0100

    upload

--- 4 Quarta/Informatica/UD 1 - Stringhe/p8.c
+++ 4 Quarta/Informatica/UD 1 - Stringhe/p8.c
@@ -1,5 +1,5 @@
-/**
- * Realizzare un programma che richiede una stringa composta da più
+/*
+Realizzare un programma che richiede una stringa composta da più
 parole, lunga massimo 100 char.
 Scansionando la stringa deve comunicare:
 
@@ -13,16 +13,110 @@ Scansionando la stringa deve comunicare:
 #include <stdlib.h>
 #include <stdio.h>
 
+int LetteraMag(char []);
+int LetteraMag2(char *);
+int NumWord(char *);
+
 int main(){
-   char *s;
-   s = malloc(sizeof(char)*10);
-
+   char *str;
+   str = malloc(sizeof(char)*100);
+   int indice_mag;
+   int num_word;
+   // acquisizione della stringa e stampa d verifica.
    printf("Inserisci la frase: ");
-   gets(s);
+   gets(str);
    fflush(stdin);
-
    printf("\n\n");
-   printf("%s", s);
+   printf("%s", str);
+   //------------------------
+   indice_mag = LetteraMag(str);
+   printf("\n\nLettera che compare piu' volte: %c", indice_mag);
+   indice_mag = LetteraMag2(str);
+   printf("\n\nLettera che compare piu' volte: %c", indice_mag);
+   //numero di parole presenti nella stringa
+   num_word = NumWord(str);
+   if(num_word != 0)
+      printf("\n\nLa stringa contiene %d parole", num_word);
+   else  
+      printf("\n\nLa stringa non contiene parole");
+
+
 
    return(0);
+}
+
+int LetteraMag(char s[]){
+   int alfa[26];
+   int i;
+   int indice;
+   int Max;
+
+   for(i=0; i<26; i++){
+      alfa[i] = 0;
+   }
+
+   for(i=0; s[i] != '\0'; i++ )
+      alfa[s[i] - 'a'] ++;
+
+   /*for(i=0; i<26; i++)
+      printf("%3d ", alfa[i]);
+   */
+   indice = 0;
+   Max = alfa[0];
+
+   for(i=1; i<26; i ++){
+      if(alfa[i] > Max){
+         Max = alfa[i];
+         indice =  i;
+      }
+   }
+
+   return(indice + 'a');
+   
+}
+
+int LetteraMag2(char *s){
+   int alfa[26];
+   int i;
+   int indice;
+   int Max;
+
+   for(i=0; i<26; i++){
+      alfa[i] = 0;
+   }
+
+   for(i=0; *(s+i) != '\0'; i++ )
+      alfa[*(s+i) - 'a'] ++;
+
+   /*for(i=0; i<26; i++)
+      printf("%3d ", alfa[i]);
+   */
+   indice = 0;
+   Max = alfa[0];
+
+   for(i=1; i<26; i ++){
+      if(alfa[i] > Max){
+         Max = alfa[i];
+         indice =  i;
+      }
+   }
+
+   return(indice +'a');
+   
+}
+
+int NumWord(char *s){
+   int i;
+   int cnt;
+
+   cnt=0;
+   if(*s != '\0'){
+      cnt = 1;
+
+      for(i=0; *(s+i) != '\0'; i++){
+         if(*(s+i) == ' ')
+            cnt++;
+      }
+   }
+   return(cnt);
 }
