168d43029a3f9ed855c0c7001e393ddf9a558a81
Francesco Pradella <50577204+CescoP81@users.noreply.github.com>
2021-11-08 10:05:07 +0100

    up 3i

---
+++ 3 Terza/Informatica/UD 2 - C Basics/p1.c
@@ -0,0 +1,16 @@
+/**
+*	Author: Pradella F.
+*	Date: 08.11.2021
+*	Description: Programma di prova.
+*/
+#include <stdio.h>
+
+// { -> shift+alt-gr+[ oppure alt+1 2 3 (tastierino numerico)
+// } -> shift+alt-gr+] oppure alt+1 2 5 (tastierino numerico)
+
+int main()
+{
+	printf("Hello World...");
+	
+	return(0);
+}
