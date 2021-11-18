422f506aba96fff9c6c5d5b329b59ebb8dbe570f
Francesco Pradella <50577204+CescoP81@users.noreply.github.com>
2021-11-18 12:49:15 +0100

    up 4i

---
+++ 4Quarta/Informatica/UD 1 - Stringhe/p14.c
@@ -0,0 +1,80 @@
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DIM 20
+void InputStringa(char []);
+char* NuovaStringa(char [], char*);
+void NuovaStringa_2(char [], char**);
+
+
+int main(){
+   char stringa[DIM];
+   char* pstringa;
+   char* pstringa2;
+
+   InputStringa(stringa);
+   printf("stringa inserita: %s", stringa);
+
+   pstringa = NuovaStringa(stringa, pstringa);
+   printf("\n\nStringa allocata/copiata: %s", pstringa); 
+
+   NuovaStringa_2(stringa, &pstringa2);
+   printf("\n\nStringa 2: %s", pstringa2);
+
+return(0);   
+}
+
+void InputStringa(char str[]){
+
+   printf("Inserisci una stringa (max 19 caratteri): ");
+   scanf("%s", str);
+   fflush(stdin);
+
+}
+
+char* NuovaStringa(char str[], char* pstr){ //str sempre 20, pstr dimensione stringa e contenuto
+   int i;
+   int cnt;
+
+   //1. calcolare lunghezza reale della stringa in str
+   i = 0;
+   cnt = 0;
+   while(str[i]!= '\0'){
+      cnt++;
+      i++;
+   }
+   //2. allocazione della reale lunghezza della stringa in pstr + '\0'
+   printf("%d", cnt);
+   pstr =(char *) malloc(sizeof(char) * (cnt+1));
+
+   //3. copia dei caratteri da str a pstr
+   for(i=0; i<cnt; i++){
+      *(pstr+i) = str[i];
+   }
+   *(pstr+i) = '\0';
+   return(pstr);
+}
+
+void NuovaStringa_2(char str[], char** ppstr){
+   int i;
+   int cnt;
+
+   //1. calcolare lunghezza reale della stringa in str
+   i = 0;
+   cnt = 0;
+   while(str[i]!= '\0'){
+      cnt++;
+      i++;
+   }
+
+   // 2.
+   *ppstr = malloc(sizeof(char) * (cnt+1));
+
+   // 3.
+   for(i=0; i<cnt; i++){
+      *(*ppstr+i) = str[i];
+   }
+   *(*ppstr+i) = '\0';
+   //printf("%s", *ppstr);
+}
