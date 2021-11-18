0d70daa890eace375f4b5b9f116ec3ce3e46e7bf
Francesco Pradella <50577204+CescoP81@users.noreply.github.com>
2021-11-18 13:05:23 +0100

    up 4i

---
+++ 4Quarta/Informatica/UD 1 - Stringhe/p15.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+   int a;
+   int *pa;
+   int **ppa;
+
+
+   a = 5;
+   pa = &a;
+   ppa = &pa;
+
+   printf("&a: %d e a: %d\n\n", &a, a);
+   printf("&pa: %d - pa: %d -> *pa: %d\n\n", &pa, pa, *pa);
+   printf("ppa: %d -> **ppa: %d",ppa, *(*ppa));
+   return(0);
+}
