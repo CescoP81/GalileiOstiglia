7197a7bdbb24c217561858b3d817f02b61bc877d
Francesco Pradella <50577204+CescoP81@users.noreply.github.com>
2021-11-29 11:54:49 +0100

    Upload 3i

---
+++ 3Terza/Informatica/UD 4 - Iterative/p03.c
@@ -0,0 +1,5 @@
+/* Realizza un programma ce richiede un numero in input,
+   il programma deve visualizzare tutti i divisori ed al termine comunicare 
+   quanti divisori ha trovato. se il numero di divisori è inferiore o uguale a 2
+   allora il numero inserito è primo
+*/
