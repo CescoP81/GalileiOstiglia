9109e7b3f3373b7a9ae262ad85b5f34e29eaecbc
Francesco Pradella <50577204+CescoP81@users.noreply.github.com>
2022-01-14 12:28:41 +0100

    up 4i

---
+++ 4Quarta/TPSI/UD 2 - Java/ese1.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class ese1{
+   public static void main(String[] args) throws IOException{
+      System.out.println("Hello World...");
+      System.out.println("Ciao Mondo...");
+
+      int i;
+      for(i=1; i<=10; i++){
+         System.out.print(i+" ");
+      }
+
+      System.out.print("\n\n");
+      for(i=1; i<=10; i++){
+         if(i%2 == 0)
+            System.out.print(i+" Pari\n");
+         else
+            System.out.print(i+" Dispari\n");
+      }
+
+      System.out.print("\n\n");
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      String s;
+      s = br.readLine();
+      
+      System.out.print("Stringa: "+s);
+   }
+}
