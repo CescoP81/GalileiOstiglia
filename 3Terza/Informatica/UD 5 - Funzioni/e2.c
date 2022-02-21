077c75fdfec49fb761bb7f7958729434f9e97af6
Francesco Pradella <50577204+CescoP81@users.noreply.github.com>
2022-02-21 10:35:09 +0100

    upload 3i

---
+++ 3Terza/Informatica/UD 5 - Funzioni/e2.c
@@ -0,0 +1,10 @@
+/*
+   Realizzare un programma C che richiede due numeri da tastiera, quindi prevede le seguenti funzoni:
+   - printDivisori -> Riceve un numero e visualizza tutti i suoi divisori.
+   - getNumeroDivisori -> Riceve un valore e restituisce il numero di divisori trovati.
+   - getSommaDivisori -> Riceve un numero e restituisce la somma dei suoi divisori.
+   - verifayAmicali -> riceve due numeri e verifica se sono 'amicali'; si dicono amicali se la somma dei
+      divisori del primo numero(escluso se stesso) è uguale al secondo E se la somma dei divisori del secondo(escluso se stesso) numero è uguale al primo.
+
+   Creare la sezione dei prototipi con relativi commenti, seguita dal main, ed infine la sezione di sviluppo delle funzioni.
+/*
