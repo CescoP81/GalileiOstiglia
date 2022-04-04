9e5c77842faf37223243ada3a579d0ef8a2259b4
Francesco Pradella <50577204+CescoP81@users.noreply.github.com>
2022-04-04 11:22:39 +0200

    up

--- 3Terza/Informatica/UD 5 - Funzioni/e10.c
+++ 3Terza/Informatica/UD 5 - Funzioni/e10.c
@@ -8,7 +8,22 @@
 #define MIN 5
 #define MAX 20
 
+/**
+ * @brief Restituisce il numero inserito dall'utente in un intervallo spcificato.
+ * @param int Estremo inferiore.
+ * @param int Estremo superiore.
+ * 
+ * @return int Numero inserito dall'utente.
+ */
 int getUserNumber(int, int);
+/**
+ * @brief Verifica se il numero random della AI è uguale a quello scelto dall'utente.
+ * @param int Numero estratto random dalla AI.
+ * @param int Numero scelto dall'utente.
+ * @param int* Riferimento al contatore dei tentativi.
+ * 
+ * @return int Esito del confronto.
+ */
 int checkNumber(int, int, int*);
 
 int main(){
@@ -21,18 +36,8 @@ int main(){
    indovinato = 0;
    cntTentativi = 0;
    do{
-      cntTentativi++;
       sceltoRandom = MIN + rand()%(MAX-MIN+1);
-      if(sceltoRandom == numeroUtente){
-         printf("L'AI ha indovinato il numero in %d tentativi!", cntTentativi);
-         indovinato = 1;
-      }
-      else{
-         if(sceltoRandom > numeroUtente)
-            printf("L'AI ha sparato troppo alto...\n");
-         else
-            printf("L'AI ha sparato troppo basso...\n");
-      }
+      indovinato = checkNumber(sceltoRandom, numeroUtente, &cntTentativi);
    }while(indovinato == 0);
    return(0);
 }
@@ -45,4 +50,19 @@ int getUserNumber(int _min, int _max){
       fflush(stdin);
    }while(n<_min || n>_max);
    return(n);   
+}
+
+int checkNumber(int _random, int _utente, int *_cnt){
+   *_cnt = *_cnt + 1;
+   if(_utente == _random){
+         printf("L'AI ha indovinato il numero in %d tentativi!", *_cnt);
+         return(1);
+      }
+   else{
+      if(_random > _utente)
+         printf("L'AI ha sparato troppo alto(%d)...\n", _random);
+      else
+         printf("L'AI ha sparato troppo basso(%d)...\n", _random);
+   }
+   return(0);
 }
