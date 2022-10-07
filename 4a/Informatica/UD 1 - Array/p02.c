8867f9741f8a05d9465c0b5f32a37a69f8c89b49
Francesco Pradella <info@francescopradella.it>
2022-10-07 12:41:46 +0200

    up 4i

--- 4a/Informatica/UD 1 - Array/p02.c
+++ 4a/Informatica/UD 1 - Array/p02.c
@@ -4,9 +4,14 @@
     - Caricamento del vettore da tastiera, con valori interi, univoci!
     - Calcolo della media dei valori PARI e dei valori DISPARI.
     - Determinazione se compaiono più valori pari o dispari.
+
+    - Copia tra vettori di stessa dimensione.
+    - Moltiplica vettore per fattore x.
+    - Ricerca valore nel vettore e restituzione prima posizione valida.
+    - 
 */
 #include <stdio.h>
-#define DIM 5
+#define DIM 6
 
 /**
  * @brief Funzione che inizializza un vettore di interi con valori univoci.
@@ -20,6 +25,22 @@ void inputVettoreUnivoco(int[], int);
  * @param int Dimensione del vettore.
  * */
 void stampaVettore(int[], int);
+/**
+ * @brief Funzione che calcola la media dei valori pari o dispari del vettore.
+ * @param int[] Vettore da utilizzare.
+ * @param int Dimensione del vettore.
+ * @param int Flag 0/Pari - 1/Dispari.
+ * @return float Media dei valori calcolati.
+*/
+float mediaValori(int[], int, int);
+
+/**
+ * @brief Funziona che determina se nel vettore compaiono più valori pari o dispari
+ * @param int[] Vettore da utilizzare
+ * @param int Dimensione del vettore
+ * @return int Flag 0/Pari 1/Dispari -1/Ugual numero pari e dispari.
+*/
+int contaPariContaDispari(int[], int);
 
 int main(){
     int vet[DIM];
@@ -27,6 +48,18 @@ int main(){
     inputVettoreUnivoco(vet, DIM);
     printf("Vettore inserito: ");
     stampaVettore(vet, DIM);
+    printf("\n\n");
+
+    printf("Media dei valori pari: %.2f\n", mediaValori(vet, DIM, 0));
+    printf("Media dei valori dispari: %.2f\n", mediaValori(vet, DIM, 1));
+    printf("\n\n");
+
+    if(contaPariContaDispari(vet,DIM) == 1)
+        printf("Nel vettore sono presenti più valori dispari.\n");
+    if(contaPariContaDispari(vet,DIM) == 0)
+        printf("Nel vettore sono presenti più valori pari.\n");
+    if(contaPariContaDispari(vet,DIM) == -1)
+        printf("Nel vettore sono presenti tanti pari quanti dispari.\n");
     return(0);
 }
 
@@ -56,4 +89,35 @@ void stampaVettore(int _v[], int _d){
     int i;
     for(i=0; i<_d; i++)
         printf("%3d", _v[i]);
+}
+
+float mediaValori(int _v[], int _d, int _pd){
+    int somma=0;
+    int i;
+    int cnt = 0;
+
+    for(i=0; i<_d; i++){
+        if(_v[i]%2 == _pd){
+            somma = somma + _v[i];
+            cnt++;
+        }
+    }
+    return((float)somma/cnt);
+}
+
+int contaPariContaDispari(int _v[], int _d){
+    int i;
+    int cntPari, cntDispari;
+
+    cntPari = 0;
+    cntDispari = 0;
+    for(i=0; i<_d; i++){
+        if(_v[i]%2 == 0)
+            cntPari++;
+        else
+            cntDispari++;
+    }
+    if(cntPari == cntDispari) return(-1);
+    if(cntPari > cntDispari) return(0);
+    else return(1);
 }
