178cad4c9305f78fc5e8ee16cf28cedebdce44ed
Francesco Pradella <info@francescopradella.it>
2022-10-08 11:06:05 +0200

    up 5i

---
+++ 5a/Informatica/UD 1 - Basic PHP mySQL/p03.php
@@ -0,0 +1,12 @@
+step 1:
+    - creare un file SQL con il DDL di una tabella per memorizzare i dati di ipotetici libri
+    Titolo, Autore, Pagine, Prezzo, Rank (da 1 a 5)
+    - Entrare in PHPMYADMIN, selezionare database scuola2223, lanciare il DDL nella tab "SQL"
+
+step 2:
+    - Popolare la tabella con alcune righe, inserire con codice SQL 
+    INSERT INTO table_name(fileds) VALUES(valori)
+
+Step 3:
+    - creare una pagina PHP libri.php che visualizzi la tabella utilizzando i framework
+    BootStrap
