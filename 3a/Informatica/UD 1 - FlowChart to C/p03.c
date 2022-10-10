906f19cfafc8433b1b1e2bdec4b283f2a62c4146
Francesco Pradella <info@francescopradella.it>
2022-10-10 08:32:54 +0200

    ip 3ia

---
+++ 3a/Informatica/UD 1 - FlowChart to C/p03.c
@@ -0,0 +1,7 @@
+/*  Data: 10/10/22
+    Author: Francesco Pradella
+    Filename: p03.c
+    Abstracty: Realizzare un algoritmo che richiede un numero intero, quindi
+    visualizza tutti i divisori di tale numero, determina quanti divisori sono stati
+    trovati e la somma di tali divisori.
+*/
