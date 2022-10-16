fc2dc7ccd37209da52bb857620c4b9511aaa0e5d
Francesco Pradella <info@francescopradella.it>
2022-10-16 10:40:58 +0200

    upload

--- 5a/Informatica/UD 1 - Basic PHP mySQL/studenti.php
+++ 5a/Informatica/UD 1 - Basic PHP mySQL/studenti.php
@@ -1,24 +1,72 @@
 <?php
+/* Questa pagina PHP esegue le seguenti operazioni sulla tabella 'studenti' creata
+    con il codice SQL presente nel file p01.sql
+    - Visualizzazione della tabella con formattazione Bootstrap
+    - Visualizzazione del form per l'inserimento di un nuovo studente(record)
+    - Aggiunta del nuovo record alla tabella 'studenti'
+
+    Richiede:
+        - File head.html e foot.html
+        - File mylib.php posto nella sottocartella 'required'.
+*/
     if(isset($_REQUEST['scelta'])) $sc = $_REQUEST['scelta'];
     else $sc = null;
     require("required/mylib.php");
     require("../head.html");
+    
+    //echo("<a href=\"studenti.php?scelta=formStudente\">Nuovo Studente</a> ");
+    //echo("<a href=\"studenti.php?scelta=showStudenti\">Visualizza studenti</a>");
+    //echo("<br /><br />");
 
-    echo("<a href=\"studenti.php?scelta=formStudente\">Nuovo Studente</a> ");
-    echo("<a href=\"studenti.php?scelta=showStudenti\">Visualizza studenti</a>");
-    echo("<br /><br />");
+    echo("<nav class=\"navbar navbar-expand-lg bg-light\">
+        <div class=\"container-fluid\">
+            <!--<a class=\"navbar-brand\" href=\"#\">Navbar</a>
+            <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarNav\" aria-controls=\"navbarNav\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+            <span class=\"navbar-toggler-icon\"></span>
+            </button>-->
+            <div class=\"collapse navbar-collapse\" id=\"navbarNav\">
+                <ul class=\"navbar-nav\">
+                    <li class=\"nav-item\">
+                        <a class=\"nav-link active\" aria-current=\"page\" href=\"studenti.php?scelta=formStudente\">Nuovo Studente</a>
+                    </li>
+                    <li class=\"nav-item\">
+                        <a class=\"nav-link active\" aria-current=\"page\" href=\"studenti.php?scelta=showStudenti\">Vedi Studenti</a>
+                    </li>
+                    <!--<li class=\"nav-item\">
+                    <a class=\"nav-link\" href=\"#\">Pricing</a>
+                    </li>
+                    <li class=\"nav-item\">
+                    <a class=\"nav-link disabled\">Disabled</a>
+                    </li>-->
+                </ul>
+            </div>
+        </div>
+    </nav>");
 
     switch($sc){
         case "formStudente":{
-            echo("DEVO MOSTRARE IL FORM PER I DATI DI UNO STUDENTE...");
+            // per i componenti dei form, la guida è qui: https://getbootstrap.com/docs/5.2/forms/overview/
+            //echo("DEVO MOSTRARE IL FORM PER I DATI DI UNO STUDENTE...");
             echo("<form action=\"studenti.php\">
-                <b>Cognome:</b><input type=\"text\" name=\"nomeS\"><br />
-                <b>Nome:</b><input type=\"text\" name=\"cognomeS\"><br />
-                <b>Classe:</b><input type=\"text\" name=\"classeS\"><br />
+                <div class=\"mb-3\">
+                    <label for=\"exampleFormControlInput1\" class=\"form-label\">Cognome</label>
+                    <input type=\"text\" name=\"cognomeS\" class=\"form-control\" id=\"exampleFormControlInput1\" placeholder=\"Cognome...\">
+                </div>
+
+                <div class=\"mb-3\">
+                    <label for=\"exampleFormControlInput2\" class=\"form-label\">Nome</label>
+                    <input type=\"text\" name=\"nomeS\" class=\"form-control\" id=\"exampleFormControlInput2\" placeholder=\"Nome...\">
+                </div>
+
+                <div class=\"mb-3\">
+                    <label for=\"exampleFormControlInput3\" class=\"form-label\">Classe</label>
+                    <input type=\"text\" name=\"classeS\" class=\"form-control\" id=\"exampleFormControlInput3\" placeholder=\"Classe...\">
+                </div>
 
                 <input type=\"hidden\" name=\"scelta\" value=\"addStudente\">
-
-                <input type=\"submit\" value=\"Aggiungi nuovo Studente\">
+                <div class=\"col-auto\">
+                    <button type=\"submit\" class=\"btn btn-primary mb-3\">Aggiungi Studente</button>
+                </div>
             </form>");
             break;
         }
