71e2261902e0193123df7b8b58115624bb435313
Francesco Pradella <info@francescopradella.it>
2022-10-20 12:55:03 +0200

    upload

---
+++ 4a/Informatica/UD 1 - Array/p04.c
@@ -0,0 +1,15 @@
+/**
+ * Realizzare un algoritmo che tramite funzioni apposite realizzi i seguenti punti su una matrice
+ * di interi 5x5
+   - [ ] Caricamento con valori casuali, il range deve essere passato come parametro.
+   - [ ] Stampa della matrice
+   - [ ] Ricerca posizione del valore maggiore (attenzione a come restituire gli indici al main)
+   - [ ] Visualizzazione del triangolo superiore, inferiore, solo diagonale principale (scelta dell'utente fatta nel main)
+   - [ ] Ordinamento della matrice in modo crescente
+   - [ ] Scambio delle righe con le colonne (la primma riga diventa la prima colonna e così via...)
+*/
+#include <stdio.h>
+
+int main(){
+    return(0);
+}
