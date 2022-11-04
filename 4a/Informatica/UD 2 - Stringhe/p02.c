34c5f93df058702ce82489eb271353d3140dbdfe
Francesco Pradella <info@francescopradella.it>
2022-11-04 12:25:01 +0100

    update

--- 4a/Informatica/UD 2 - Stringhe/p02.c
+++ 4a/Informatica/UD 2 - Stringhe/p02.c
@@ -4,21 +4,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void initStringa(char*);
+void printStringa(char*);
 int getStringLength(char*);
 
 int main(){
     char *str;
+    // alloco memoria per 100 caratteri e assegno l'indirizzo iniaziale al puntatore str
     str = malloc(sizeof(char)*100);
 
-    printf("Inserisci stringa: ");
-    scanf("%s", str);
-    fflush(stdin);
-
-    printf("Inserita: %s", str);
+    initStringa(str);
+    printStringa(str);
     printf("\nLunghezza: %d", getStringLength(str));
     return(0);
 }
 
+void initStringa(char *_str){
+    printf("Inserisci la stringa: ");
+    scanf("%s", _str);
+    fflush(stdin);
+}
+void printStringa(char *_str){
+    printf("%s", _str);
+}
 int getStringLength(char *_str){
     int i;
     i=0;
