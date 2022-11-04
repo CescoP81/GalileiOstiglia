93708544b144f6026c5be764fade3a6efef48b7b
Francesco Pradella <info@francescopradella.it>
2022-11-04 12:43:48 +0100

    update

--- 4a/Informatica/UD 2 - Stringhe/p02.c
+++ 4a/Informatica/UD 2 - Stringhe/p02.c
@@ -7,6 +7,10 @@
 void initStringa(char*);
 void printStringa(char*);
 int getStringLength(char*);
+void printReversedString(char*);
+
+void stampaStringaRicorsiva(char*);
+void stampaStringaReversedRicorsiva(char*);
 
 int main(){
     char *str;
@@ -16,6 +20,12 @@ int main(){
     initStringa(str);
     printStringa(str);
     printf("\nLunghezza: %d", getStringLength(str));
+    printf("\nRovescia: ");
+    printReversedString(str);
+    printf("\n\n");
+    stampaStringaRicorsiva(str);
+    printf("\n");
+    stampaStringaReversedRicorsiva(str);
     return(0);
 }
 
@@ -33,4 +43,26 @@ int getStringLength(char *_str){
     while(*(_str+i) != '\0')
         i++;
     return(i);
+}
+void printReversedString(char *_str){
+    int lung;
+    int i;
+
+    lung = getStringLength(_str);
+    for(i=lung-1; i>=0; i--){
+        printf("%c", *(_str+i));
+    }
+}
+
+void stampaStringaRicorsiva(char *_str){
+    if(*_str != '\0'){
+        printf("%c", *_str);     
+        stampaStringaRicorsiva(_str+1);   
+    }
+}
+void stampaStringaReversedRicorsiva(char *_str){
+    if(*_str != '\0'){
+        stampaStringaReversedRicorsiva(_str+1);
+        printf("%c", *_str);    
+    }
 }
