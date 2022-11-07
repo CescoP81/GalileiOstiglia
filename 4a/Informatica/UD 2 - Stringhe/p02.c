7cfc00e7f9bf890d5d3286122ef2694b86c036f4
Francesco Pradella <info@francescopradella.it>
2022-11-07 22:16:40 +0100

    update

--- 4a/Informatica/UD 2 - Stringhe/p02.c
+++ 4a/Informatica/UD 2 - Stringhe/p02.c
@@ -12,10 +12,16 @@ void printReversedString(char*);
 void stampaStringaRicorsiva(char*);
 void stampaStringaReversedRicorsiva(char*);
 
+char getCharAt(char*, int);
+int countChar(char*, char);
+
+int checkPalindroma(char*);
+
 int main(){
     char *str;
     // alloco memoria per 100 caratteri e assegno l'indirizzo iniaziale al puntatore str
     str = malloc(sizeof(char)*100);
+    char c;
 
     initStringa(str);
     printStringa(str);
@@ -26,6 +32,15 @@ int main(){
     stampaStringaRicorsiva(str);
     printf("\n");
     stampaStringaReversedRicorsiva(str);
+    printf("Carattere in posizione 3: %c", getCharAt(str, 3));
+    printf("\n");
+    c = 'e';
+    printf("Il carattere %c e' presente %d volta/te", c, countChar(str, c));
+    printf("\n");
+    if(checkPalindroma(str))
+        printf("La parola %s e' palindroma!", str);
+    else
+        printf("La parola %s non e' palindroma", str);
     return(0);
 }
 
@@ -53,7 +68,6 @@ void printReversedString(char *_str){
         printf("%c", *(_str+i));
     }
 }
-
 void stampaStringaRicorsiva(char *_str){
     if(*_str != '\0'){
         printf("%c", *_str);     
@@ -65,4 +79,30 @@ void stampaStringaReversedRicorsiva(char *_str){
         stampaStringaReversedRicorsiva(_str+1);
         printf("%c", *_str);    
     }
+}
+char getCharAt(char *_str, int _index){
+    if(_index>0 && _index<=getStringLength(_str)){
+        return(*(_str + _index - 1));
+    }
+    else
+        return('?');
+}
+int countChar(char *_str, char _src){
+    int i;
+    int cnt;
+
+    cnt = 0;
+    for(i=0; i<getStringLength(_str); i++){
+        if(*(_str+i) == _src)
+            cnt++;
+    }
+    return(cnt);
+}
+int checkPalindroma(char *_str){
+    int i;
+    for(i=0; i<getStringLength(_str); i++){
+        if(*(_str+i) != *(_str+getStringLength(_str)-i-1))
+            return(0);
+    }
+    return(1);
 }
--- 4a/Informatica/UD 2 - Stringhe/readme.md
+++ 4a/Informatica/UD 2 - Stringhe/readme.md
@@ -10,3 +10,15 @@ Realizzare un programma per la gestione di una stringa inserita da tastiera di m
 - [ ] countChar() Conta e restituisce il numero di occorrenze di un carattere in una stringa.
 - [ ] checkPalindroma() Verifica si una parola è palindroma oppure no; 1 Vero 0 Falso. 
 
+
+**p02.c**  
+Realizzare un programma per la gestione di una stringa inserita da tastiera (stringa allocata massimo 100byte), utilizzando un puntatore a stringa.
+- [x] initStringa() Richiede l'input di una stringa da tastiera.
+- [x] printStringa() Stampa la stringa passata.
+- [x] getStrLength() Ritorna la lunghezza della stringa passata.
+- [x] printReversedString Stampa la stringa al contrario.
+- [x] stampaStringaRicorsiva() stampa una stringa in maniera ricorsiva.
+- [x] stampaStringaReversedRicorsiva() stampa una stringa al contrario in modo ricorsivo.
+- [x] getCharAt() Ritorna il carattere della stringa alla posizione scelta; -1 se la posizione non esiste.
+- [x] countChar() Conta e restituisce il numero di occorrenze di un carattere in una stringa.
+- [x] checkPalindroma() Verifica si una parola è palindroma oppure no; 1 Vero 0 Falso.
