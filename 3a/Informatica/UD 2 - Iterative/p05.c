0a62c444781073c4a9204848feb9fbd8f9fd8baf
Francesco Pradella <info@francescopradella.it>
2022-11-19 08:25:16 +0100

    update

--- 3a/Informatica/UD 2 - Iterative/p05.c
+++ 3a/Informatica/UD 2 - Iterative/p05.c
@@ -10,3 +10,42 @@ mentre # per tutte le altre.
 */
 #include <stdio.h>
 #include <stdlib.h>
+
+#define MAX 10
+
+int main(){
+    int riga, colonna;  // variabili per i cicli delle tabelline.
+    int riga_min, riga_max; // estremi delle tabelline scelte.
+
+    printf("Inserisci tabellina inferiore: ");
+    scanf("%d", &riga_min);
+    fflush(stdin);
+
+    printf("Inserisci tabellina superiore: ");
+    scanf("%d", &riga_max);
+    fflush(stdin);
+
+    if(riga_min < riga_max){
+        // eseguo la creazione delle tabellini
+        riga=1;
+        while(riga <= MAX){
+            colonna = 1;
+            while(colonna <= MAX){
+                if(riga>=riga_min && riga<=riga_max){
+                    printf("%4d", (riga * colonna));
+                }
+                else{
+                    printf("   #");
+                }
+                colonna = colonna + 1;
+                
+            }
+            printf("\n");
+            riga = riga + 1;
+        }
+    }
+    else{
+        printf("tabelline non visualizzabili, indici non validi.");
+    }
+    return(0);
+}
