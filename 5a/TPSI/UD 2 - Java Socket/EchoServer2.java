ef7b7be0ee4b1e806093720fba5b3f9d6dba1605
Francesco Pradella <info@francescopradella.it>
2022-11-29 22:51:06 +0100

    up

--- 5a/TPSI/UD 2 - Java Socket/EchoServer2.java
+++ 5a/TPSI/UD 2 - Java Socket/EchoServer2.java
@@ -7,24 +7,24 @@ class EchoServer2 {
         System.err.println(Arrays.toString(args));
     }
     public static void main(String ars[]) throws IOException {
-        int port = 9876;
-        String lineInSS;
-        String lineOutSS;
-        ServerSocket ss = new ServerSocket(port); // crea server socket
+        int port = 9876;                                                    // Porta su cui risponde il ServerSocket
+        String lineInSS;                                                    // Stringa per la riga letta dal ServerSocket
+        String lineOutSS;                                                   // Stringa da pubblicare sul ServerSocket
+        ServerSocket ss = new ServerSocket(port);                           // Crea server socket che "ascolta" sulla port
 		
         log("accepting connections on port " + port);
-        Socket socket = ss.accept(); // aspettiamo connessione client
-        InputStream is = socket.getInputStream();
-        OutputStream os = socket.getOutputStream();
-        BufferedReader bf = new BufferedReader(new InputStreamReader(is));
+        Socket socket = ss.accept();                                        // aspettiamo connessione client -> Telnet 'open localhost 9876'
+        InputStream is = socket.getInputStream();                           // Creo un input stream da SocketServer
+        OutputStream os = socket.getOutputStream();                         // Creo un output stream su SocketServer
+        BufferedReader bf = new BufferedReader(new InputStreamReader(is));  // Creo un BufferedReader per leggere stringhe dall'input stream.
         
-        do{
-            os.write("hello, I'm EchoServer!\r\n".getBytes());
-            os.write("Insert a string: ".getBytes());
-            lineInSS = new String(bf.readLine());
-            lineOutSS = "You said "+lineInSS+"? ;)\r\n";
-            os.write(lineOutSS.getBytes()); // write reponse
-        }while(!lineInSS.equals("ciao!"));
+        do{                                                                 // entro in un ciclo while finche ServerSocket non riceve "ciao!"
+            os.write("hello, I'm EchoServer!\r\n".getBytes());              // ServerSocket manda al client un messaggio (\n line feed, \r carriage return)
+            os.write("Insert a string: ".getBytes());                       // ServerSocket manda al client un messaggio
+            lineInSS = new String(bf.readLine());                           // Leggo la stringa inserita dal client fino al carattere 'invio'
+            lineOutSS = "You said "+lineInSS+"? ;)\r\n";                    // Creo la stringa da rimandare al client.
+            os.write(lineOutSS.getBytes());                                 // Scrivo la risposta lineOutSS sul ServerSocket
+        }while(!lineInSS.equals("ciao!"));                        // Verifico che ServerSocket non abbia ricevuto la stringa 'ciao!' che lo fa terminare.
         log("have a nice day... " + socket);
     }
 }
