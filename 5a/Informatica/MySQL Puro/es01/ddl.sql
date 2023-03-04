4b1de6e09d1f43c12fffe79e25ba549526e9180a
Francesco Pradella <info@francescopradella.it>
2023-03-04 10:10:05 +0100

    upl

---
+++ 5a/Informatica/MySQL Puro/es01/ddl.sql
@@ -0,0 +1,33 @@
+CREATE TABLE auto(
+    id integer unsigned auto_increment,
+    targa char(7) not null,
+    marca varchar(15) not null,
+    modello varchar(25) not null,
+
+    primary key(id)
+);
+
+CREATE TABLE cliente(
+    id integer unsigned auto_increment,
+    cognome varchar(20) not null,
+    nome varchar(20) not null,
+    indirizzo varchar(50),
+    telefono varchar(10),
+    cartaCredito varchar(16) not null,
+
+    primary key(id)
+);
+
+CREATE TABLE noleggio(
+    id integer unsigned auto_increment,
+    dataNoleggio date not null,
+    idAuto integer unsigned,
+    idCliente integer unsigned,
+    dataResa date not null,
+    dataResaEffettiva date,
+    kmPercorsi integer unsigned,
+
+    primary key(id),
+    foreign key (idAuto) references auto(id),
+    foreign key (idCliente) references cliente(id)
+);
---
+++ 5a/Informatica/MySQL Puro/es01/dml.sql
@@ -0,0 +1,60 @@
+INSERT INTO auto(targa, marca, modello)
+VALUES('AB123CD', 'FIAT', 'Panda 1.2 GPL');
+
+INSERT INTO auto(targa, marca, modello)
+VALUES('AC123CF', 'FIAT', 'Punto 2.0 TDI');
+
+INSERT INTO auto(targa, marca, modello)
+VALUES('AC123CG', 'FIAT', 'Doblò 1.8 TDI');
+
+INSERT INTO auto(targa, marca, modello)
+VALUES('AB034LJ', 'FIAT', 'Bravo 2.0 Benzina');
+
+INSERT INTO auto(targa, marca, modello)
+VALUES('GF233ER', 'VOLKSWAGEN', 'Passat 2.0 TDI');
+
+INSERT INTO auto(targa, marca, modello)
+VALUES('GF233ET', 'VOLKSWAGEN', 'Polo 1.6 TDI');
+
+INSERT INTO auto(targa, marca, modello)
+VALUES('CD090AC', 'VOLKSWAGEN', 'Touran 2.0 TDI');
+
+INSERT INTO auto(targa, marca, modello)
+VALUES('GF213EA', 'VOLKSWAGEN', 'ID.3');
+
+/* ------------------------------------------ */
+
+INSERT INTO cliente(cognome, nome, indirizzo, telefono, cartaCredito)
+VALUES('Rossi','Giuseppe','','','1111222233334444');
+
+INSERT INTO cliente(cognome, nome, indirizzo, telefono, cartaCredito)
+VALUES('Rossi','Luigia','','','1111222233334444');
+
+INSERT INTO cliente(cognome, nome, indirizzo, telefono, cartaCredito)
+VALUES('Verdi','Alberta','via America 4b, Ostiglia','','1111222233334444');
+
+INSERT INTO cliente(cognome, nome, indirizzo, telefono, cartaCredito)
+VALUES('Verdi','Maria','via Sistri 1, Quistello','','1111222233334444');
+
+INSERT INTO cliente(cognome, nome, indirizzo, telefono, cartaCredito)
+VALUES('Orlandi','Simone','via Ronchi 16, Sustinente','','1111222233334444');
+
+/* ------------------------------------------ */
+
+INSERT INTO noleggio(dataNoleggio, idAuto, idCliente, dataResa, dataResaEffettiva, kmPercorsi) 
+VALUES('2023/01/25', 1, 1, '2023/01/30', '2023/01/31', 250);
+
+INSERT INTO noleggio(dataNoleggio, idAuto, idCliente, dataResa, dataResaEffettiva, kmPercorsi) 
+VALUES('2023/02/03', 1, 1, '2023/02/05', '2023/02/05', 100);
+
+INSERT INTO noleggio(dataNoleggio, idAuto, idCliente, dataResa, dataResaEffettiva, kmPercorsi) 
+VALUES('2023/02/03', 3, 2, '2023/02/05', '2023/02/08', 300);
+
+INSERT INTO noleggio(dataNoleggio, idAuto, idCliente, dataResa, dataResaEffettiva, kmPercorsi) 
+VALUES('2023/02/15', 3, 3, '2023/02/25', '2023/02/28', 575);
+
+INSERT INTO noleggio(dataNoleggio, idAuto, idCliente, dataResa, dataResaEffettiva, kmPercorsi) 
+VALUES('2023/03/01', 4, 5, '2023/02/05', '2023/02/08', 125);
+
+INSERT INTO noleggio(dataNoleggio, idAuto, idCliente, dataResa, dataResaEffettiva, kmPercorsi) 
+VALUES('2023/03/11', 7, 5, '2023/03/15', '2023/03/18', 230);
