c24bf2f1e3608125e753b20d9d2af7313cb57bc0
Francesco Pradella <info@francescopradella.it>
2023-04-24 23:13:18 +0200

    upl

--- 5a/Informatica/UD 5 - GD Library/grafico.php
+++ 5a/Informatica/UD 5 - GD Library/grafico.php
@@ -19,10 +19,10 @@ $labels = Array('Gen','Feb','Mar','Apr','Mag','Giu','Lug','Ago','Set','Ott','Nov
 $width = 640;           // larghezza totale dell'area immagine.
 $height = 480;          // altezza totale dell'area immagine.
 
-$marginTop = 30;        // margine superiore al grafico.
+$marginTop = 50;        // margine superiore al grafico.
 $marginLeft = 150;      // margine sinistro al grafico.
 $marginBottom = 130;     // margine inferiore al grafico.
-$marginRight = 50;      // margine destro al grafico.
+$marginRight = 70;      // margine destro al grafico.
 
 $availableHeight = $height - $marginTop - $marginBottom;        // calcolo dell'altezza massima del grafico, necessario per le proporzioni.
 $numberOfValues = count($values);                               // determino quanti elementi devo graficare.
@@ -30,6 +30,7 @@ $maxValue = $values[0];                                         // determino il 
 for($i=1; $i<$numberOfValues; $i++)
     if($values[$i] > $maxValue)
         $maxValue = $values[$i];
+$maxValue += 5;
 
 /* ### CREAZIONE DELLA TELA GRAFICA E ALLOCAZIONE DI ALCUNI COLORI */
 $im = imageCreate($width, $height);                             // creo l'area grafica.
@@ -39,51 +40,64 @@ $gray = imageColorAllocate($im, 200,200,200);
 $red = imageColorAllocate($im, 200,0,0);
 $green = imageColorAllocate($im, 0,200,0);
 $blue = imageColorAllocate($im, 0,0,200);
+/**
+ * Funzione di prova con phpDocumentor
+ * @param integer $x Variabile 1 della funzione
+ * @param integer $y Variabile 2 della funzioneù
+ * @return integer Moltiplicazione tra x e y.
+ */
+function miaFunzione($x, $y){
+    return($x*$y);
+}
 
 // disegno il perimetro della png.
 imageRectangle($im, 0, 0, $width-1, $height-1, $black);
 
 // disegno il perimetro del grafico.
-imageRectangle($im, $marginLeft, $marginTop, $width-$marginRight, $height-$marginBottom, $gray);
+imageRectangle($im, $marginLeft, $marginTop, $width-$marginRight, $height-$marginBottom, $blue);
 
 // grafico le tacche sull'asse x del grafico.
 $dx = ($width-$marginLeft-$marginRight)/($numberOfValues-1);
 $x = $marginLeft;
 for($i = 0; $i<$numberOfValues; $i++){
-    imageLine($im, $x, $height-$marginBottom-5, $x, $height-$marginBottom+5, $gray);
+    imageLine($im, $x, $height-$marginBottom-5, $x, $height-$marginBottom+5, $red);
     $x = $x + $dx;
 }
+// */
 
 /*  grafico i punti all'interno del grafico.
     la coordinata Y del punto la devo determinare come proporzione del valore massimo presente nel vettore con il valore massimo di altezza disponibile.
     $availableHeight:$maxValue = $y:$values[$i] -> $y = $availableHeight*$values[$i]/$maxValue
     Devo poi individuarla all'interno dell'area grafica sapendo che l'asse Y è invertito rispetto al normale.
 */
+
 $x = $marginLeft;
 for($i = 0; $i<$numberOfValues; $i++){
     $y = $availableHeight*$values[$i]/$maxValue;
 
     imageFilledEllipse($im, $x, $height-$marginBottom-$y, 10,10, $green);
     $x = $x + $dx;
-}
+}//*/
 
 /* Grafico le linee tra i punti, il ciclo che si muove sul vettore dei valori deve fermarsi sul penultimo valore,
     perchè una linea parte dal punti i e arriva al punto i+1. */
-$x = $marginLeft;
+
+    $x = $marginLeft;
 for($i = 0; $i<$numberOfValues-1; $i++){
     $y1 = $availableHeight*$values[$i]/$maxValue;
     $y2 = $availableHeight*$values[$i+1]/$maxValue;
 
     imageLine($im, $x, $height-$marginBottom-$y1, $x+$dx, $height-$marginBottom-$y2, $green);
     $x = $x + $dx;
-}
+}//*/
 
 /* Visualizzo le stringhe come didascalie dell'asse x. */
+
 $x = $marginLeft;
 for($i = 0; $i<$numberOfValues; $i++){
     imageString($im, 4, $x-12, $height-$marginBottom+10, $labels[$i], $black);
     $x = $x + $dx;
-}
+}// */
 
 imagePNG($im);
 imageDestroy($im);
