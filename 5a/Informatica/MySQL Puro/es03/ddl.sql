1e087e79c5124fca11eaee3ea8646613a7a101f0
Francesco Pradella <info@francescopradella.it>
2023-05-06 10:54:43 +0200

    upd

--- 5a/Informatica/MySQL Puro/es03/ddl.sql
+++ 5a/Informatica/MySQL Puro/es03/ddl.sql
@@ -35,7 +35,7 @@ create table parcoFlora(
 
     foreign key (idSpecieFlora) references specieFlora(id),
     foreign key (idParco) references parco(id),
-    primary key(idSpecieFlora, idParco)
+    primary key (idSpecieFlora, idParco)
 );
 
 create table specieFauna(
@@ -62,3 +62,38 @@ create table esemplareFauna(
     foreign key (idSpecie) references specieFauna(id),
     primary key (id, idParco)
 )
+
+insert into tipoFlora(tipologia) values('alberi');
+insert into tipoFlora(tipologia) values('arbusti');
+insert into tipoFlora(tipologia) values('piante erbacee');
+
+insert into parco(parco, regione) values('Colli Euganei', 'Veneto');
+insert into parco(parco, regione) values('Dolomiti Bellunesi', 'Veneto');
+insert into parco(parco, regione) values('Dolomiti D\'Ampezzo', 'Veneto');
+insert into parco(parco, regione) values('Lessinia', 'Veneto');
+
+insert into parco(parco, regione) values('Adamello', 'Lombardia');
+insert into parco(parco, regione) values('Adda Nord', 'Lombardia');
+insert into parco(parco, regione) values('Adda Sud', 'Lombardia');
+insert into parco(parco, regione) values('Parco del Mincio', 'Lombardia');
+
+insert into ordineFauna (ordine) values('mammiferi');
+insert into ordineFauna (ordine) values('rettili');
+insert into ordineFauna (ordine) values('uccelli');
+
+insert into specieFlora(idTipoFlora, specie, fioritura, altreCaratteristiche) values (1, 'quercia', 'primavera', 'frutto ghianda');
+insert into specieFlora(idTipoFlora, specie, fioritura, altreCaratteristiche) values (1, 'faggio', 'autunno', 'nc');
+insert into specieFlora(idTipoFlora, specie, fioritura, altreCaratteristiche) values (1, 'acacia', 'primavera', 'pianta con spine');
+insert into specieFlora(idTipoFlora, specie, fioritura, altreCaratteristiche) values (2, 'lavanda', 'primavera', 'utile per profumi');
+insert into specieFlora(idTipoFlora, specie, fioritura, altreCaratteristiche) values (2, 'rosmarino', 'primavera', 'spezia da cucina');
+insert into specieFlora(idTipoFlora, specie, fioritura, altreCaratteristiche) values (2, 'rododendro', 'autunno', 'bacche velenose');
+insert into specieFlora(idTipoFlora, specie, fioritura, altreCaratteristiche) values (3, 'fragola di bosco', 'primavera', 'frutti piccoli');
+insert into specieFlora(idTipoFlora, specie, fioritura, altreCaratteristiche) values (3, 'margherita', 'primavera', 'nc');
+insert into specieFlora(idTipoFlora, specie, fioritura, altreCaratteristiche) values (3, 'primula', 'estate', 'nc');
+
+insert into specieFauna(idOrdine, specie, anniAdulto, nEsemplariRischio) values (1, 'scoiattolo', 3, 15);
+insert into specieFauna(idOrdine, specie, anniAdulto, nEsemplariRischio) values (1, 'volpe', 6, 30);
+insert into specieFauna(idOrdine, specie, anniAdulto, nEsemplariRischio) values (1, 'orso', 4, 25);
+insert into specieFauna(idOrdine, specie, anniAdulto, nEsemplariRischio) values (2, 'vipera', 1, 50);
+insert into specieFauna(idOrdine, specie, anniAdulto, nEsemplariRischio) values (3, 'falco', 3, 30);
+insert into specieFauna(idOrdine, specie, anniAdulto, nEsemplariRischio) values (3, 'aquila', 3, 10);
