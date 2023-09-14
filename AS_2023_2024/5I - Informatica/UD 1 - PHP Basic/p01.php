cbdeac8d2f4c3da1a28445a68afe4d563ce6f266
Francesco Pradella <info@francescopradella.it>
2023-09-14 16:17:12 +0200

    upd

--- AS_2023_2024/5I - Informatica/UD 1 - PHP Basic/p01.php
+++ AS_2023_2024/5I - Informatica/UD 1 - PHP Basic/p01.php
@@ -1,4 +1,5 @@
 <?php
+    // funzione echo() per la stampa di messaggi, stringhe da restituire al browser.
     echo("<HTML>
         <HEAD><TITLE>Prove di PHP</TITLE></HEAD>
         <BODY>
@@ -6,11 +7,12 @@
 
     echo("Hello World to 5IA...<br /><br />");
 
+    // esempio di ciclo while per stampa di 10 numeri random, da notare la sintassi praticamente identica al conosciuto C/C++.
     $cnt = 1;
     while($cnt <= 10){
         $nRandom = rand(10, 45);
         if($nRandom%2 == 0)
-            echo($nRandom. " è pari<br />");
+            echo($nRandom. " è pari<br />");           // l'operatore '.', punto, serve per concatenare variabili e stringhe.
         else
             echo($nRandom. " è dispari<br />");
         
@@ -18,8 +20,11 @@
     }
 
     echo("<br /><br />");
+
+    // esempio di come si può modificare velocemente il contenuto di una variabile da numero intero a stringa, senza casting o altro.
     $cnt = "MA che bello il PHP...";
     echo($cnt);
+
     echo("</BODY>
         </HTML>
     ");
