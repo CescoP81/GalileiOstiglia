07983d69a087a25ba50b65b161583fca25331ad7
Francesco Pradella <info@francescopradella.it>
2023-09-27 12:47:59 +0200

    upl

---
+++ AS_2023_2024/5IB - TPSI/UD 1 - CSV e JSON/p01.c
@@ -0,0 +1,5 @@
+/*
+    Applicazione per l'inserimento di 5 persone indentificate da Nome, Cognome, Residenza e memorizzate
+    su un file binario 'elenco.dat' e su un file formato CSV (Comma Separated Values).
+    IL programma utilizza Strutture, Scrittura su file binari, Scrittura su file di testo.
+*/
