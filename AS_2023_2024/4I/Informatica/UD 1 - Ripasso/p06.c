484f9e652c8380e7b6a7706c79b104ba60aacc01
Francesco Pradella <info@francescopradella.it>
2023-10-12 12:16:20 +0200

    upl

---
+++ AS_2023_2024/4I/Informatica/UD 1 - Ripasso/p06.c
@@ -0,0 +1,11 @@
+/*
+    Realizzare il programma "Catch the Code" il quale all'avvio inizializza un vettore di 5 elementi interi
+    con valori random compresi tra 0 e 9.
+    Il programma deve richiedere all'utente di indovinare i 5 valori.
+    - Richiede i 5 valori interi all'utente.
+    - Verifica i valori con quelli presenti nel vettore
+        - se il valore è esatto lo visualizza
+        - se il valore non è esatto visualizza un ?
+    - il gioco termina quando l'untente indovina tutte e 5 le cifre, comunicando anche il numero di tentativi effettuati.
+    IL PROGRAMMA E' DA REALIZZARE CON LE FUNZIONI.
+*/
