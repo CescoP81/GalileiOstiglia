d9856cd640c716a35fbe918d48a83f808b5bf105
Francesco Pradella <info@francescopradella.it>
2023-10-23 13:32:22 +0200

    upl

---
+++ AS_2023_2024/4I/Informatica/UD 2 - Stringhe/p02.c
@@ -0,0 +1,18 @@
+/*
+    creare un programma C che sfruttando la conoscenza delle stringhe permetta le seguenti
+    funzioni:
+    - [] Inizializzazione di una stringa vuota.
+    - [] Inserimento di una stringa da tastiera.
+    - [] Conteggio delle lettere della stringa.
+    - [] Conteggio di una lettera inserita da tastiera.
+    - [] Conteggio di quante vocali compaiono nella stringa.
+    - [] Sostituzione di una lettera src con una lettera sostitutiva dst.
+    - [] Visualizzazione di una stringa al contrario.
+    - [] Verifica se una parola inserita è palindroma.
+
+    Ogni singolo punto è da realizzare con una specifica funzione, relativi parametri
+    passati per valore/riferimento e con ritorno void o non-void. 
+*/
+#include <stdio.h>
+
+const int 
