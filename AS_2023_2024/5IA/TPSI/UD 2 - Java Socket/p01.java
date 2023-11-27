9a0c999abb6af779b4018cb201b22bfe965264be
Francesco Pradella <info@francescopradella.it>
2023-11-27 08:32:33 +0100

    upl

---
+++ AS_2023_2024/5IA/TPSI/UD 2 - Java Socket/p01.java
@@ -0,0 +1,25 @@
+// sezione di caricamento "import" delle varie librerie Java.
+import java.util.*;
+
+// definizione della classe pubblica generale
+// - DEVE avere lo stesso nome del file che stiamo scrivendo
+// - DEVE contenere il metodo MAIN()
+public class p01{
+    public static void main(String[] args){
+        System.out.print("Hello World");
+    }
+}
+/*
+ * Per compilare il file java il comando è:
+ * c:\>javac nomefile.java
+ * se non ci sono errori di compilazione viene prodotto il file 'nomefile.class'
+ * per eseguire il programma java il comando è:
+ * c:\>java nomefile [senza l'estensione .class]
+ */
+
+ /*
+  * COMANDI FONDAMENTALI DELLA SHELL
+  * DIR -> Visualizza la lista file/cartelle presente nel ramo del filesystem in cui viene lanciato.
+  * CD nomecartella -> Cambia la cartella corrente con quella indicata come parametro.
+  * CLS -> Clear Screen pulisce il terminale.
+  */
