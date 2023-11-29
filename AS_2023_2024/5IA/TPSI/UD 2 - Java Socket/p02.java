d8cbd9f914f214efbaf8160b7cd2153cc1c6373e
Francesco Pradella <info@francescopradella.it>
2023-11-29 15:08:36 +0100

    upd

--- AS_2023_2024/5IA/TPSI/UD 2 - Java Socket/p02.java
+++ AS_2023_2024/5IA/TPSI/UD 2 - Java Socket/p02.java
@@ -4,8 +4,15 @@
  * 
  * L'inserimento del valore realizzarlo con un BufferedReader
  */
+import java.io.*;
+
 public class p02{
-    public static void main(String[] args){
-        
+    public static void main(String[] args) throws IOException {
+        String str;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Inserisci una parola: ");
+        str = br.readLine();
+
+        System.out.print("Hai detto forse "+str+"?");
     }
 }
