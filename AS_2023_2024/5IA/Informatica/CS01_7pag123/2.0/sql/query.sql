4c35589c0d35532544cccb88a96370ba5716fedc
Francesco Pradella <info@francescopradella.it>
2024-01-16 09:23:22 +0100

    upl

---
+++ AS_2023_2024/5IA/Informatica/CS01_7pag123/2.0/sql/query.sql
@@ -0,0 +1,14 @@
+-- visualizzare tutti i reparti presenti nella città di Mantova
+SELECT rep.nomeReparto
+FROM Reparto AS rep, Citta AS c
+WHERE c.nomeCitta = 'Mantova' AND c.id = rep.idCittaReparto
+
+-- visualizzare tutti i reparti presenti nelle città di Mantova e Vicenza
+SELECT rep.nomeReparto
+FROM Reparto AS rep, Citta AS c
+WHERE c.id = rep.idCittaReparto AND (c.nomeCitta = 'Mantova' OR c.nomeCitta = 'Vicenza')
+
+-- visualizzare tutti i dipendenti che lavorano nella città di Mantova
+SELECT d.nome, d.Cognome
+FROM Citta AS c, Dipendente AS d, Reparto AS rep
+WHERE d.idReparto = rep.id AND rep.idCittaReparto = c.id AND c.nomeCitta = 'Vicenza'
