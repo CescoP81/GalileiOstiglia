ebee037c8bf633158fb0e49372c4fd72096ee193
Francesco Pradella <info@francescopradella.it>
2024-02-06 12:36:01 +0100

    upl

---
+++ AS_2023_2024/3I/Informatica/UD 4 - Funzioni/p04.c
@@ -0,0 +1,16 @@
+/*
+    Creare un programma che utilizzando funzioni appositamente create permetta
+    le seguenti operazioni:
+    - [ ] Inserimento di un valore val1 compreso tra 1 e 999.
+    - [ ] Visualizzazione di tutti i divisori di val1.
+    - [ ] Calcolo e ritorno al main della somma divisori di val1.
+    - [ ] Verifica se val1 è PRIMO (divisibile solo per 1 e per se stesso), la funzione
+    ritorna 1 se è primo, 0 se non lo è.
+    - [ ] Verifica se val1 è PERFETTO (la somma dei divisori è uguale a se stesso), la funzione
+    ritorna 1 se è perfetto, 0 se non lo è.
+    - [ ] Inserimento di un valore val2 compreso tra 1 e 999.
+    - [ ] Verifica se val1 e val2 sono numeri amici (o amicali), restituisce 1 se lo sono,
+    0 se non lo sono.
+    - [ ] Verifica se val1 è un quadrato perfetto, restituisce 1 se lo è, 0 se non lo è.
+    Quadrato perfetto: 16, 25, 36, 49, 81, etc...
+*/
