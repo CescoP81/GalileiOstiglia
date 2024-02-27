7e501b9f9af9a31529448290d677772b1d821a08
Francesco Pradella <info@francescopradella.it>
2024-02-27 09:51:16 +0100

    upd

--- AS_2023_2024/5IB - TPSI/UD 3 - Cifratura/cifratura.php
+++ AS_2023_2024/5IB - TPSI/UD 3 - Cifratura/cifratura.php
@@ -27,7 +27,31 @@ switch($sc){
             echo("Registrazione avvenuta con successo.<br />");
         else
             echo("Problema in registrazione.<br />");
-        
+
+        $db->close();
+        break;
+    }
+    case "login":{
+        $mail = $_REQUEST['mail_utente'];
+        $password = $_REQUEST['passwd_utente'];
+
+        // cifrare la password inserita dall'utente
+        $password = md5($password."xyz");
+
+        // collego il db e verifico se esiste una coppia mail-password nel database.
+        $db = new mysqli("localhost","root","","scuola2324");
+        $sql = "SELECT nome, cognome 
+                FROM utente 
+                WHERE email='$mail' AND passwd='$password'";
+        echo($sql."<br />");
+        $rs = $db->query($sql);
+        if($rs->num_rows > 0){
+            $record = $rs->fetch_assoc();
+            echo($record['nome']." ".$record['cognome']);
+        }
+        else{
+            echo("Nessun record trovato. Utente o password errati/a");
+        }
         $db->close();
         break;
     }
@@ -35,14 +59,29 @@ switch($sc){
         // caso in cui l'utente non abbia eseguito ancora nessuna operazione
         // quindi presento il form di inserimento dati da cifrare.
         echo('
-            <form action="cifratura.php" method="get">
-                Nome: <input type="text" name="nome_utente"><br />
-                Cognome: <input type="text" name="cognome_utente"><br />
-                email: <input type="text" name="mail_utente"><br />
-                password: <input type="password" name="passwd_utente"><br/>
-                <input type="hidden" name="scelta" value="registra">
-                <input type="submit" value="Registra Nuovo Utente">   
-            </form>
+            <table style="width: 100%">
+                <tr style="text-align: center;"><th>REGISTRAZIONE</th><th>LOGIN UTENTE</th></tr>
+                <tr style="text-align: center;">
+                    <td>
+                        <form action="cifratura.php" method="post">
+                            Nome: <input type="text" name="nome_utente"><br />
+                            Cognome: <input type="text" name="cognome_utente"><br />
+                            email: <input type="text" name="mail_utente"><br />
+                            password: <input type="password" name="passwd_utente"><br/>
+                            <input type="hidden" name="scelta" value="registra">
+                            <input type="submit" value="Registra Nuovo Utente">   
+                        </form>
+                    </td>
+                    <td>
+                        <form action="cifratura.php" method="post">
+                            e-mail: <input type="text" name="mail_utente"><br />
+                            password: <input type="text" name="passwd_utente"><br />
+                            <input type="hidden" name="scelta" value="login">
+                            <input type="submit" value="Esegui il Login">
+                        </form>
+                    </td>
+                </tr>
+            </table>
         ');
         break;
     }
