3cb0f58b85e99090e5accc9e1d1b2d7b160d26f1
Francesco Pradella <info@francescopradella.it>
2024-02-29 11:30:02 +0100

    upl

--- AS_2023_2024/4I/Informatica/UD 3 - File e struct/p10.c
+++ AS_2023_2024/4I/Informatica/UD 3 - File e struct/p10.c
@@ -7,18 +7,19 @@
     - Giri -> 45 o 33
     L'applicazione deve permettere le seguenti funzionalità utilizzando come archivio il file binario
     'discografia.dat':
-    - [ ] Aggiunta di un nuovo Disco alla collezione.
-    - [ ] Visualizzazione dei Disco della collezione.
-    - [ ] Ricerca di un Disco basata su Cognome Artista.
+    - [x] Aggiunta di un nuovo Disco alla collezione.
+    - [x] Visualizzazione dei Disco della collezione.
+    - [x] Ricerca di un Disco basata su Cognome Artista.
     - [ ] Ricerca di un Disco basata su Anno o intervallo anni.
     - [ ] Modifica dei dati di un Disco.
-    - [ ] Cancellazione di un Disco dall'elenco discografico.
+    - [x] Cancellazione di un Disco dall'elenco discografico.
 
     NB: il MAIN PROGRAM prevede un menu, uno switch case e la chiamata di una FUNZIONE
     per ogni punto da sviluppare.
 */
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 // struttura
 typedef struct Disco{
     char cognome[20];
@@ -43,22 +44,34 @@ typedef struct Disco{
 */
 int mainMenu();
 
+void insertNewDisco(Disco*, char*);
+void listAllDisco(Disco*, char*);
+void searchByCognome(Disco*, char*, char*);
+void deleteDisco(Disco*, char*);
+
 // MAIN PROGRAM
 int main(){
     int scelta;
     Disco* d;
+    FILE *fpout;    //utilizzo per scrivere
+    FILE *fpin;     //utilizzo per leggere
     
     d = malloc(sizeof(Disco));
     do{
         scelta = mainMenu();
         switch(scelta){
             case 1:{
+                // inserimento informazioni del disco
+                insertNewDisco(d, "discografia.dat");
                 break;
             }
             case 2:{
+                // lista dei dischi -> apro il file in lettura e leggo tutti i titolo.
+                listAllDisco(d, "discografia.dat");
                 break;
             }
             case 3:{
+                searchByCognome(d, "discografia.dat", "Vecchioni");
                 break;
             }
             case 4:{
@@ -68,6 +81,8 @@ int main(){
                 break;
             }
             case 6:{
+                printf("Cancellazione di un disco\n");
+                deleteDisco(d, "discografia.dat");
                 break;
             }
             case 0:{
@@ -88,7 +103,7 @@ int mainMenu(){
         printf("1-> Aggiunta disco\n");
         printf("2-> Lista dischi\n");
         printf("3-> Ricerca cognome\n");
-        printf("4-> Ricerca per anno");
+        printf("4-> Ricerca per anno\n");
         printf("5-> Modifica dati disco\n");
         printf("6-> Cancellazione disco\n");
         printf("0 -> USCITA\n");
@@ -96,4 +111,88 @@ int mainMenu(){
         scanf("%d", &s);
     }while(s<0 || s>6);
     return(s);
+}
+
+void insertNewDisco(Disco *_d, char *_filename){
+    FILE *fpout;
+    
+    printf("Cognome Artista: ");
+    scanf("%s", _d->cognome);
+    fflush(stdin);
+    printf("Nome Artista: ");
+    scanf("%s", _d->nome);
+    fflush(stdin);
+    printf("Titolo album: ");
+    gets(_d->titolo);
+    fflush(stdin);
+    printf("Giri: ");
+    scanf("%d", &(_d->giri));
+    fflush(stdin);
+    printf("Anno: ");
+    scanf("%d", &(_d->anno));
+    fflush(stdin);
+                
+    fpout = fopen(_filename, "ab");
+    fwrite(_d, sizeof(Disco), 1, fpout);
+    fclose(fpout);
+}
+
+void listAllDisco(Disco *_d, char *_filename){
+    FILE *fpin;
+    int cnt;
+    fpin = fopen(_filename,"rb");
+    cnt = 1;
+    while(fread(_d, sizeof(Disco), 1, fpin)){
+        printf("%d -> %s %s con: '%s' - %d giri - Anno: %d\n", cnt, _d->cognome, _d->nome, _d->titolo, _d->giri, _d->anno);
+        cnt = cnt + 1;
+    }
+    fclose(fpin);
+}
+
+void searchByCognome(Disco *_d, char *_filename, char *_cognome){
+    FILE *fpin;
+    int cnt;
+    fpin = fopen(_filename,"rb");
+    cnt = 0;
+    while(fread(_d, sizeof(Disco), 1, fpin)){
+        if(strcmp(_d->cognome, _cognome) == 0){
+            cnt = cnt + 1;
+            printf("%d -> %s %s con: '%s' - %d giri - Anno: %d\n", cnt, _d->cognome, _d->nome, _d->titolo, _d->giri, _d->anno);
+        }
+    }
+    printf("Risultati totali trovati: %d\n", cnt);
+    fclose(fpin);
+}
+
+void deleteDisco(Disco *_d, char *_filename){
+    FILE *fpin, *fpout;
+    int discoScelto;
+    int cnt;
+
+    // visualizzo la lista dischi presenti nel file, sfrutto la funzione già sviluppata.
+    listAllDisco(_d, _filename);
+    printf("Nr. disco da cancellare: ");
+    scanf("%d", &discoScelto);
+    fflush(stdin);
+
+    // copio sul file temporaneo tmp.dat tutti i disci MENO quello scelto.
+    fpin = fopen(_filename, "rb");
+    fpout = fopen("tmp.dat", "wb");
+    cnt = 0;
+    while(fread(_d, sizeof(Disco), 1, fpin)){
+        cnt = cnt + 1;
+        if(cnt != discoScelto)
+            fwrite(_d, sizeof(Disco), 1, fpout);
+    }
+    fclose(fpin);
+    fclose(fpout);
+
+    // ricopio tmp su discografia.
+    fpin = fopen("tmp.dat", "rb");
+    fpout = fopen(_filename, "wb");
+    while(fread(_d, sizeof(Disco), 1, fpin)){
+        fwrite(_d, sizeof(Disco), 1, fpout);
+    }
+    fclose(fpin);
+    fclose(fpout);
 }
