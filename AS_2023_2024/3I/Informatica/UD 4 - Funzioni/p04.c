495a1d4ced34c9d4845827977d178a1e11f15ecc
Francesco Pradella <info@francescopradella.it>
2024-04-09 11:13:41 +0200

    upd

--- AS_2023_2024/3I/Informatica/UD 4 - Funzioni/p04.c
+++ AS_2023_2024/3I/Informatica/UD 4 - Funzioni/p04.c
@@ -6,12 +6,12 @@
     - [x] Calcolo e ritorno al main della somma divisori di val1.
     - [x] Verifica se val1 è PRIMO (divisibile solo per 1 e per se stesso), la funzione
     ritorna 1 se è primo, 0 se non lo è.
-    - [ ] Verifica se val1 è PERFETTO (la somma dei divisori è uguale a se stesso), la funzione
+    - [x] Verifica se val1 è PERFETTO (la somma dei divisori è uguale a se stesso), la funzione
     ritorna 1 se è perfetto, 0 se non lo è. [6 è perfetto -> 1+2+3=6]
-    - [ ] Inserimento di un valore val2 compreso tra 1 e 999.
+    - [x] Inserimento di un valore val2 compreso tra 1 e 999.
     - [ ] Verifica se val1 e val2 sono numeri amici (o amicali), restituisce 1 se lo sono,
     0 se non lo sono.
-    - [ ] Verifica se val1 è un quadrato perfetto, restituisce 1 se lo è, 0 se non lo è.
+    - [x] Verifica se val1 è un quadrato perfetto, restituisce 1 se lo è, 0 se non lo è.
     Quadrato perfetto: 16, 25, 36, 49, 81, etc...
 */
 #include <stdio.h>
