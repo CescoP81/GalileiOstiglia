a01766e5d9f144e2b8b30de514ae5aacb9f74d81
Francesco Pradella <info@francescopradella.it>
2024-04-30 11:13:38 +0200

    upl

---
+++ AS_2023_2024/3I/Informatica/UD 5 - Array/p03.c
@@ -0,0 +1,9 @@
+/*
+Realizzare un programma che esegue le seguenti operazioni, in sequenza, utilizzando funzioni e vettori.
+1. Inizializza con valori random, compresi tra 1 e 20, un vettore di 10 elementi.
+2. Visualizza il vettore dalla cella 0 alla cella Dim-1.
+3. Calcola e restituisce la media del vettore.
+4. Calcola e restituisce quanti valori sono superiori alla media.
+5. Richiede un valore X e se il valore della cella è maggiore di X allora lo sottrae, altrimenti lo aggiunge.
+6. Visualizza a video tutti i valori pari e nella riga sotto tutti i valori dispari.
+*/
