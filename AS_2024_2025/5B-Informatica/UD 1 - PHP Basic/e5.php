ea1a882077789e729fed65962dd224c086c5ad5e
Francesco Pradella <info@francescopradella.it>
2024-09-30 11:37:25 +0200

    upl

---
+++ AS_2024_2025/5B-Informatica/UD 1 - PHP Basic/e5.php
@@ -0,0 +1,9 @@
+<?php
+    /*
+    Realizzare uno script PHP che riceve un valore intero nel vettore
+    delle request con chiave 'Val'. 
+    Lo script calcola e comunica il successivo numero primo.
+    Formattare correttamente la pagina secondo lo standard HTML
+    per visualizzare il risultato.
+    */
+?>
