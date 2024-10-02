9c3ee31862945169b80dbaa6a6490656cbd3727a
Francesco Pradella <info@francescopradella.it>
2024-10-02 11:08:47 +0200

    upl

---
+++ AS_2024_2025/5A-Informatica/UD 1 - PHP Basics/e4.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+    Realizzare uno script PHP che riceve due valori interi nel vettore
+    delle request con chiave 'Val' e 'Direzione'. 
+    Se 'Direzione' vale 1 allora lo script comunica il numero primo successivo 
+    al valore di 'Val'; se invece vale -1 allora comunica il precedente numero primo
+    rispetto a 'Val' se esiste prima di arrivare a 0.
+    */
+
+
+?>
