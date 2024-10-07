7026d03fb926576481f815cc37cbabbfce3433b3
Francesco Pradella <info@francescopradella.it>
2024-10-07 08:55:12 +0200

    upl

---
+++ AS_2024_2025/5A-Informatica/UD 2 - mySQL Basic/ddl.sql
@@ -0,0 +1,11 @@
+-- Questo file con linguaggio mySQL contiene SOLO
+-- la definizione delle tabelle (cioè come si chiamano e quali campi sono presenti)
+
+CREATE TABLE Artista(
+    id integer unsigned auto_increment,
+    nome varchar(20) not null,
+    cognome varchar(20) not null,
+    nazione char(2),
+
+    primary key(id)
+)ENGINE=InnoDB;
---
+++ AS_2024_2025/5A-Informatica/UD 2 - mySQL Basic/dml.sql
@@ -0,0 +1,11 @@
+-- Contiene le INSERT iniziali per popolare il database e poter
+-- eseguire alcune query di controllo.
+
+INSERT INTO Artista(nome, cognome, nazione)
+VALUES('Riccardo', 'Cocciante', 'IT');
+
+INSERT INTO Artista(nome, cognome, nazione)
+VALUES('Laura', 'Pausini', 'IT');
+
+INSERT INTO Artista(nome, cognome, nazione)
+VALUES('David', 'Bowie', 'UK');
