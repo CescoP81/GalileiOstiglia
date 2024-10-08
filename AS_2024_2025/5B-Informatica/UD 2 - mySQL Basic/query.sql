c1fee7a6a3d87c7f8dd35601b160e5b84d01916d
Francesco Pradella <info@francescopradella.it>
2024-10-08 11:35:27 +0200

    upl

---
+++ AS_2024_2025/5B-Informatica/UD 2 - mySQL Basic/query.sql
@@ -0,0 +1,26 @@
+-- QUesto file contiene tutte le query sviluppate ed eseguibili
+-- sulla banca dati in uso.
+
+-- Q1: Visualizzare tutti gli artisti presenti nel database
+SELECT * FROM Artista;
+-- Q1.1
+SELECT nome, cognome, nazione FROM Artista;
+-- Q1.2
+SELECT cognome,nome,nazione FROM Artista;
+
+-- Q2: Visualizzare tutti i brani presenti.
+SELECT * FROM Brano;
+-- Q2.1
+SELECT Brano.*, Artista.*
+FROM Brano, Artista;
+-- Q2.3
+SELECT Brano.*, Artista.*
+FROM Brano, Artista
+WHERE Brano.idArtista = Artista.id;
+
+-- Q3: Visualizzare tutti i brani e relativo autore, ordinando i risultati
+-- per cognome dell'autore.
+SELECT Brano.*, Artista.*
+FROM Brano, Artista
+WHERE Brano.idArtista = Artista.id
+ORDER BY Artista.cognome;
