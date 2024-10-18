284c24de41220d0d0055ac5cc9f383aede9d3a0a
Francesco Pradella <info@francescopradella.it>
2024-10-18 13:56:31 +0200

    upl

---
+++ AS_2024_2025/4I-Informatica/UD 2 - Stringhe/e5.c
@@ -0,0 +1,12 @@
+/*
+Realizzare un programma C che richieda l'inserimento di due parole st1 e st2 quindi
+sviluppare i seguenti punti con relative funzioni:
+1. Visualizza la stringa più lunga tra le due e calcola la differenza di lunghezza.
+2. Verifica per ogni parola se è palindroma oppure no.
+3. Per ogni parola conta e visualizza il numero di vocali presenti.
+4. Calcola e restituisce al main la vocale più presente (sia per st1 che per st2).
+5. Visualizza le lettere di st1 che compaiono anche in st2.
+5. Visualizza le lettere di st2 che NON compaiono in st1.
+6. Cifra la stringa st1 con un fattore k scelta compreso tra 5 e 10; la stringa cifrata
+    viene stampata nel main e NON deve essere modificata st1.
+*/
