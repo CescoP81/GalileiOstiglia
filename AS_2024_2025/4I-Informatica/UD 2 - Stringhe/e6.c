22773be1a0e7185b5ffb944c00cfe7762faa5bc9
Francesco Pradella <info@francescopradella.it>
2024-10-29 12:12:21 +0100

    upl

---
+++ AS_2024_2025/4I-Informatica/UD 2 - Stringhe/e6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+const int DIM_STRINGA = 20;
+const int NR_STRINGHE = 5;
+int main(){
+    /*dichiaro una matrice di caratteri formata da 5 righe e 20 colonne,
+    ogni riga conterrà una stringa di massimo 19 caratteri più il terminatore.
+    Utilizzo la matrice come un elenco di parole da analizzare, modificare, etc...*/
+    char parole[NR_STRINGHE][DIM_STRINGA];
+
+    int i;
+    for(i=0; i<NR_STRINGHE; i++){
+        printf("Inserisci la %d parola: ", i+1);
+        gets(parole[i]);
+    }
+
+    for(i=0; i<NR_STRINGHE; i++){
+        printf("%s ", parole[i]);
+    }
+    return(0);
+}
