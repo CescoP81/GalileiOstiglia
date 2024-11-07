ada90f886ef2748484cfafdfec30df38c9eb5afe
Francesco Pradella <info@francescopradella.it>
2024-11-07 10:55:43 +0100

    upl

--- AS_2024_2025/5B-Informatica/cs02pag122/1.0/php/index.php
+++ AS_2024_2025/5B-Informatica/cs02pag122/1.0/php/index.php
@@ -1,4 +1,14 @@
 <?php
-echo("<!DOCTYPE html>");
-echo("Da qui inizia l'interfaccia della nostra applicazione...");
+// includo la libreria con funzioni necessarie all'interfaccia.
+require('../include/lib.php');
+// richiamo la funzione per la creazione della sezione iniziale delle pagine
+writeHeader();
+
+    // << your code start here >>
+        echo('<div class="alert alert-success">Pare funzionare tutto!</div>');
+
+    // << end fo your code >>
+    
+// richiamo la funzione per la creazione della sezione finale delle pagine.
+writeFooter();
 ?>
