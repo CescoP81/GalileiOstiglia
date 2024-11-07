47d89ca419d677a7a3d8ec749b9d59dbe2bad467
Francesco Pradella <info@francescopradella.it>
2024-11-07 11:18:17 +0100

    upd

--- AS_2024_2025/5B-Informatica/cs02pag122/1.0/include/lib.php
+++ AS_2024_2025/5B-Informatica/cs02pag122/1.0/include/lib.php
@@ -29,4 +29,29 @@ function writeFooter(){
             </html>');
     return;
 }
+
+function writeMenu(){
+    echo('
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>    
+    ');
+    return;
+}
 ?>
--- AS_2024_2025/5B-Informatica/cs02pag122/1.0/php/index.php
+++ AS_2024_2025/5B-Informatica/cs02pag122/1.0/php/index.php
@@ -3,12 +3,13 @@
 require('../include/lib.php');
 // richiamo la funzione per la creazione della sezione iniziale delle pagine
 writeHeader();
+writeMenu();
 
     // << your code start here >>
         echo('<div class="alert alert-success">Pare funzionare tutto!</div>');
 
     // << end fo your code >>
-    
+
 // richiamo la funzione per la creazione della sezione finale delle pagine.
 writeFooter();
 ?>
