dd85c9aeca95046651b692042a752803a9596704
Francesco Pradella <info@francescopradella.it>
2024-11-07 17:10:30 +0100

    upd

--- AS_2024_2025/5B-Informatica/cs02pag122/1.0/include/lib.php
+++ AS_2024_2025/5B-Informatica/cs02pag122/1.0/include/lib.php
@@ -47,6 +47,9 @@ function writeMenu(){
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">HOME</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item dropdown">
@@ -62,7 +65,8 @@ function writeMenu(){
                 </div>
             </div>
         </nav>    
-    ');
+    '); //*/
+
     return;
 }
 ?>
