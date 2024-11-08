594e0ad8f5d937642761b1de84b899d62bd30ad7
Francesco Pradella <info@francescopradella.it>
2024-11-08 13:17:01 +0100

    upl

---
+++ AS_2024_2025/5A-Informatica/cs02pag188/1.0/sql/dml.sql
@@ -0,0 +1,8 @@
+-- QUESTO FILE CONTIENE L'INSERIMENTO DELLE RIGHE INIZIALI NELLE RELATIVE TABELLE
+
+INSERT INTO regione(nome) VALUES('Lombardia');
+INSERT INTO regione(nome) VALUES('Veneto');
+INSERT INTO regione(nome) VALUES('Piemonte');
+INSERT INTO regione(nome) VALUES('Emilia Romagna');
+INSERT INTO regione(nome) VALUES('Liguria');
+INSERT INTO regione(nome) VALUES('Trentino Alto Adige');
