1888926ce619f839de761c1b836c42806fe905e4
Francesco Pradella <info@francescopradella.it>
2024-11-08 13:44:49 +0100

    upl

---
+++ AS_2024_2025/5A-Informatica/cs02pag188/1.0/include/lib.php
@@ -0,0 +1,19 @@
+<?php
+// questo file contiene funzioni e variabili utili in più pagine per non doverle ricopiare.
+function writeHeader(){
+    echo('
+        <!DOCTYPE HTML>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>Caso studio 02 Pag 188</title>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+            </head>
+            <body>
+                <div class="container">
+    ');
+    return;
+}
+
+?>
