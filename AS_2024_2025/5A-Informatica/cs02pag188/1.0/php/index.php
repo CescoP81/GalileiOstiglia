a5ff882bdd534916893896ff6f53d1d45e3b7802
Francesco Pradella <info@francescopradella.it>
2024-11-08 13:55:22 +0100

    upl

---
+++ AS_2024_2025/5A-Informatica/cs02pag188/1.0/php/index.php
@@ -0,0 +1,13 @@
+<?php
+require('../include/lib.php');
+// richiamo la header per generale il codice iniziale della pagina.
+writeHeader();
+
+    // >> il tuo codice inizia qui.
+    echo('<div class="alert alert-success">Alert di prova</div>');
+
+    // >> il tuo codice finisce qui.
+    
+// richiamo la footer per chiudere in modo corretto le pagine.
+writeFooter();
+?>
