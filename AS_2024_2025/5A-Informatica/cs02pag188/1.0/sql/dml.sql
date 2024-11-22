aedf70656de1814267049a4072534f44e7249a14
Francesco Pradella <info@francescopradella.it>
2024-11-22 13:10:48 +0100

    upd

--- AS_2024_2025/5A-Informatica/cs02pag188/1.0/sql/dml.sql
+++ AS_2024_2025/5A-Informatica/cs02pag188/1.0/sql/dml.sql
@@ -7,3 +7,10 @@ INSERT INTO regione(nome) VALUES('Emilia Romagna');
 INSERT INTO regione(nome) VALUES('Liguria');
 INSERT INTO regione(nome) VALUES('Trentino Alto Adige');
 INSERT INTO regione(nome) VALUES('Lazio');
+
+INSERT INTO prodotto(descrizione, prezzoUnitario, note)
+VALUES('Forchetta Carta', 9.55, 'Pacchi da 200');
+INSERT INTO prodotto(descrizione, prezzoUnitario, note)
+VALUES('Bicchiere 200ml', 7.55, 'Pacchi da 200');
+INSERT INTO prodotto(descrizione, prezzoUnitario, note)
+VALUES('Piatto Carta', 12.45, '');
