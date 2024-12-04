823c5f6fb4fe2df333407ec6e960883dde20871a
Francesco Pradella <info@francescopradella.it>
2024-12-04 21:17:55 +0100

    upd

--- AS_2024_2025/3I-Sistemi/UD 1- Assembly 8086/e08.asm
+++ AS_2024_2025/3I-Sistemi/UD 1- Assembly 8086/e08.asm
@@ -1,3 +1,18 @@
 ; Realizza un programma Assembly che imposta il registro AX con valore 0005h, il registro BX
 ; con valore 0004h e calcola in CX il prodotto di AX*BX con somme successive.
 ; Good Luck.
+.model small
+.stack 200h
+.data
+.code
+
+MAIN:
+    .startup
+    mov ax, @data
+    mov ds, ax
+
+    mov ah, 4ch
+    int 21h
+    .exit
+END
+    
