e4024950fb8c082d2f4b48a6c770fac91623a5e7
Francesco Pradella <info@francescopradella.it>
2024-12-05 17:19:16 +0100

    upl

--- AS_2024_2025/5A-Informatica/UD 1 - PHP Basics/e4.php
+++ AS_2024_2025/5A-Informatica/UD 1 - PHP Basics/e4.php
@@ -6,6 +6,39 @@
     al valore di 'Val'; se invece vale -1 allora comunica il precedente numero primo
     rispetto a 'Val' se esiste prima di arrivare a 0.
     */
+$valore = $_REQUEST['Val'];
+$direzione = $_REQUEST['Direzione'];
 
+$cnt = 0;
+$div = 1;
+
+    if($direzione == 1){
+        // cerco il numero primo successivo a quello inserito come valore
+        do{
+            $valore++;
+            $cnt = 0;
+            $i = 1;
+            while($i <= $valore){
+                if($valore%$i == 0)
+                    $cnt++;
+                $i++;
+            }
+        }while($cnt>2);
+        echo("Il valore PRIMO successivo a quello inserito è: ".$valore);
+    }
+    if($direzione == -1){
+        // cerco il numero primo precedente a quello inserito come valore
+        do{
+            $valore--;
+            $cnt = 0;
+            $i = 1;
+            while($i <= $valore){
+                if($valore%$i == 0)
+                    $cnt++;
+                $i++;
+            }
+        }while($cnt>2);
+        echo("Il valore PRIMO precedente a quello inserito è: ".$valore);
+    }
 
 ?>
