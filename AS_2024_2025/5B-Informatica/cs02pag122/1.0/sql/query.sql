0e09d0a8cc121688f0223afba9d4d4a8bbe1b122
Francesco Pradella <info@francescopradella.it>
2024-12-05 11:36:01 +0100

    upd

--- AS_2024_2025/5B-Informatica/cs02pag122/1.0/sql/query.sql
+++ AS_2024_2025/5B-Informatica/cs02pag122/1.0/sql/query.sql
@@ -19,3 +19,41 @@ FROM acquisto as a
 INNER JOIN prodotto as p on p.id = a.idProdotto
 INNER JOIN magazzino as m on a.idMagazzino = m.id
 ORDER BY a.dataAcquisto, a.prezzoAcquisto desc;
+
+--Determinare il numero di prodotti presenti nel database
+SELECT count(id)
+FROM prodotto
+--Determinare il prezzo minore dei prodotti presenti
+SELECT min(prezzoAcquisto)
+FROM acquisto;
+--Detterminare il prezzo massimo di acquisto presente nel database
+SELECT max(prezzoAcquisto)
+FROM acquisto;
+--Determinare il prezzo medio degli acquisti presenti nel database
+SELECT avg(prezzoAcquisto)
+FROM acquisto;
+--
+SELECT prezzoAcquisto
+FROM acquisto
+ORDER BY prezzoAcquisto desc
+LIMIT 1;
+--Visualizzare tutti gli acquisti con prezzo inferiore o uguale al prezzo di acquisto medio
+SELECT acquisto.*
+FROM acquisto
+WHERE prezzoAcquisto <= (
+    SELECT avg(prezzoAcquisto)FROM acquisto
+    ); 
+--Come query precedente ma visualizzando i dati del prodotto
+SELECT p.descrizione as prodotto,m.nome as magazzino, a.dataAcquisto, a.quantitaAcquisto, a.prezzoAcquisto
+FROM acquisto as a, prodotto as p, magazzino as m
+WHERE a.prezzoAcquisto <=(SELECT avg(prezzoAcquisto) FROM acquisto)
+AND p.id = a.idProdotto
+AND a.idMagazzino = m.id;
+
+--Visualizzare tutti gli ordini effettuati dal magazzino 'Mantova 1';
+SELECT p.descrizione, a.dataAcquisto,a.prezzoAcquisto,a.quantitaAcquisto
+FROM prodotto as p, acquisto as a, magazzino as m
+WHERE p.id = a.idProdotto 
+AND a.idMagazzino = m.id
+AND m.nome = 'Mantova 1';
+
