3a6b0b6a5f3963763cd1d14f51278af0fe61f1a8
Francesco Pradella <info@francescopradella.it>
2024-12-09 13:14:56 +0100

    upl

---
+++ AS_2024_2025/4I-Informatica/UD 3 - Struct/e05.c
@@ -0,0 +1,5 @@
+/*
+Realizzare un elenco di studenti, massimo 10, dove ogni studente è identificato da
+cognome, nome, da_di_nascita, classe. La data di nascita sia strutturata come giorno, mese, anno.
+Utilizzare il concetto di strutture innestate.
+*/
