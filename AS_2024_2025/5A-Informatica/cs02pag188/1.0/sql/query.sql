d0937420fc5c939c12807dc3738f78c40abbe436
Francesco Pradella <info@francescopradella.it>
2024-12-13 13:37:41 +0100

    upd

--- AS_2024_2025/5A-Informatica/cs02pag188/1.0/sql/query.sql
+++ AS_2024_2025/5A-Informatica/cs02pag188/1.0/sql/query.sql
@@ -12,3 +12,24 @@ FROM prodotto;
 -- Q3: come query sopra ma scambiando ordine degli attributi.
 SELECT descrizione, prezzoUnitario, id
 FROM prodotto;
+
+-- Q4: Visualizza tutte le regioni presenti a DB
+SELECT *
+FROM regione;
+
+-- Q5: visualizza tutti gli ufficio presenti a DB
+SELECT *
+FROM ufficio;
+
+-- Q6: Come sopra ma sfrutto il concetto di JOIN e relazioni
+-- per avere il nome della regione anziché il suo id.
+SELECT ufficio.*, regione.nome
+FROM ufficio, regione
+WHERE ufficio.idRegione = regione.id;
+
+-- Q7: Come sopra ma sfrutto il concetto di JOIN e relazioni
+-- per avere il nome della regione anziché il suo id e elimino dalla
+-- visuale le chiavi esterne e primary in regione.
+SELECT u.id, u.citta, u.indirizzo, u.telefono, u.email, r.nome AS regione
+FROM ufficio AS u, regione AS r
+WHERE u.idRegione = r.id;
