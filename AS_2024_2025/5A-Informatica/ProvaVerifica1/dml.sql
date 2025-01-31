1cad9c28e4dce1d74c302bd5b53a56cb6a41030d
Francesco Pradella <info@francescopradella.it>
2025-01-31 13:35:39 +0100

    up

--- AS_2024_2025/5A-Informatica/ProvaVerifica1/dml.sql
+++ AS_2024_2025/5A-Informatica/ProvaVerifica1/dml.sql
@@ -1,7 +1,7 @@
 INSERT INTO Razza(denominazione, paeseOrigine)
 VALUES('mammifero', 'Senegal'),
 ('rettile', 'Australia'),
-('uccello', 'Italia');
+('volatile', 'Italia');
 
 INSERT INTO Zona(denominazione)
 VALUES ('tropicale'),
@@ -10,4 +10,16 @@ VALUES ('tropicale'),
 ('zona asiatica');
 
 INSERT INTO Volontariato(Cognome, Nome, statoServizio)
-VALUES ('Mario', 'Rossi', 'SN')
+VALUES ('Rossi', 'Mario', 'SN'),
+('Bing', 'Qi Ling', 'JR'),
+('Elkadiri', 'Aziz', 'SN');
+
+INSERT INTO Animale(Nome, idRazza, idZona, idVolontariato)
+VALUES ('jeppino', 2, 3, 2),
+('gianfranco', 3, 1, 3),
+('david' , 1, 4, 1);
+
+INSERT INTO visita(dataVisita, referto, costo, idAnimale)
+VALUES ('2025-2-14', 'deceduto', 104.69, 2),
+('2025-4-21', 'in salute', 420.12, 1),
+('2023-12-25', 'normale', 32.99, 3);
