b91e39d7cab6123b54f5cc3f31de2d3c1b4c8771
Francesco Pradella <info@francescopradella.it>
2025-01-31 12:59:45 +0100

    up

---
+++ AS_2024_2025/5A-Informatica/ProvaVerifica1/ddl.sql
@@ -0,0 +1,43 @@
+CREATE TABLE Razza(
+    id integer unsigned auto_increment,
+    denominazione varchar(30) not null,
+    paeseOrigine varchar(20) not null,
+
+    primary key (id)
+)ENGINE=InnoDB;
+
+CREATE TABLE Zona(
+    id integer unsigned auto_increment,
+    denominazione varchar(30) not null,
+
+    primary key (id) 
+)ENGINE=InnoDB;
+
+CREATE TABLE Vonlontario(
+    id integer unsigned auto_increment,
+    Cognome varchar(20) not null,
+    Nome varchar(20) not null,
+    statoServizio char(2) not null,
+
+    primary key (id)
+)ENGINE=InnoDB;
+
+CREATE TABLE Animale(
+    id integer unsigned auto_increment,
+    Nome varchar(20) not null,
+
+    primary key (id),
+    foreign key (idRazza) references Razza(id)
+    foreign key (idZona) references Zona(id)
+    foreign key (idVolontario) references Volontario(id)
+)ENGINE=InnoDB;
+
+CREATE TABLE Visita(
+    id integer unsigned auto_increment,
+    dataVisita date not null,
+    referto varchar(50) not null,
+    costo float not null,
+
+    primary key(id),
+    foreign key (idAnimale) references Animale(id)
+)
