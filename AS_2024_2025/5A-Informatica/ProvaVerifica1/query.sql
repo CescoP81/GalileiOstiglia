b124528053973feb93eae628a88d0e58b612f1cd
Francesco Pradella <info@francescopradella.it>
2025-02-07 13:30:56 +0100

    upd

--- AS_2024_2025/5A-Informatica/ProvaVerifica1/query.sql
+++ AS_2024_2025/5A-Informatica/ProvaVerifica1/query.sql
@@ -35,3 +35,36 @@ SELECT a.nome, r.denominazione AS 'Razza', z.denominazione AS 'Zona', vi.dataVis
 FROM animale AS a, zona AS z, visita AS vi, volontario AS vo, razza AS r
 WHERE r.id = a.idRazza AND z.id = a.idZona AND a.id = vi.idAnimale
     AND a.idVolontario = vo.id AND vo.cognome = 'Verdi' AND vo.nome = 'Bruno';
+
+-- Q6: Selezionare la spesa complessiva di ogni singolo volontario nell’anno 2023.
+SELECT vo.cognome, vo.nome, SUM(vi.costo) AS 'Totale'
+FROM volontario AS vo, animale AS a, visita AS vi 
+WHERE vo.id = a.idVolontario AND a.id = vi.idAnimale
+    AND vi.dataVisita>='2023-01-01' AND vi.dataVisita<='2023-12-31';
+GROUP BY vo.id;
+
+-- Q7: Selezionare per ogni animale il numero di visite effettuate e il costo totale.
+SELECT a.nome, COUNT(vi.id) AS 'Numero visite', SUM(vi.costo) AS 'Costo totale'
+FROM animale AS a, visita AS vi 
+WHERE a.id = vi.idAnimale 
+GROUP BY a.id;
+
+-- Q8: Selezionare tutti gli animali che hanno un totale di spesa effettuata maggiore a 100€.
+SELECT a.nome, r.denominazione AS 'Razza', SUM(vi.costo) AS 'Totale'
+FROM animale AS a, razza AS r, visita AS vi 
+WHERE a.id=v.idAnimale AND a.idRazza=r.id 
+GROUP BY a.id 
+HAVING totale>100;
+
+-- QUERY SIMIL VERIFICA --
+
+-- Q9: Visualizzare il costo medio delle visite presenti
+SELECT AVG(costo)
+FROM visita;
+
+-- Q10: Selezionare tutte le visite indicando a che animale sono associate, che abbiano un costo
+--     maggiore del costo medio delle visite presenti.
+SELECT a.nome, vi.costo 
+FROM animale AS a, visita AS vi 
+WHERE a.id=vi.idAnimale 
+    AND vi.costo > (SELECT AVG(visita.costo) FROM visita);
