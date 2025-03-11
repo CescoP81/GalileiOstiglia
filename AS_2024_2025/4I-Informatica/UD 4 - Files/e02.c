41bc07506ac93e1a946597cb25c0e9423d0f74b4
Francesco Pradella <info@francescopradella.it>
2025-03-11 13:22:46 +0100

    upl

---
+++ AS_2024_2025/4I-Informatica/UD 4 - Files/e02.c
@@ -0,0 +1,9 @@
+/**
+ * Realizzare sulla base della struttura dell'esercizio 1 un piccolo programma che gestendo un file
+ * di contatti permetta di realizzare le seguenti operazioni
+ * 1. Aggiunta di un nuovo nominativo (senza controllo di esistenza)[nomeFile, struttura da scrivere].
+ * 2. Visualizzazione di i contatti presenti nel file. [nomeFile]
+ * 3. Ricerca di contatti per anno di nascita (mostra tutti quelli dell'anno indicato). [nomeFile, annoDiRicerca]
+ * 
+ * Il programma è da svilupparsi con uno switch-case, per ogni case viene richiamata una funzione. 
+ */
