6221c33d75ed71e7c957fbcb1626d3b119d1e9d5
Francesco Pradella <info@francescopradella.it>
2025-03-14 12:46:51 +0100

    upd

---
+++ AS_2024_2025/5A-Informatica/03-14 - ER/ddl.sql
@@ -0,0 +1,44 @@
+-- PRIMO CASO CON DUE TABELLE PARTENZA E ARRIVO
+create table partenza(
+    id integer unsigned auto_increment,
+    nomeCitta varchar(30),
+    nomeStazione varchar(30),
+    primary key(id)
+);
+create table arrivo(
+    id integer unsigned auto_increment,
+    nomeCitta varchar(30),
+    nomeStazione varchar(30),
+    primary key(id)
+);
+create table viaggio(
+    id integer unsigned auto_increment,
+    dataViaggio date
+    oraViaggio time
+    idPartenza integer unsigned,
+    idArrivo integer unsigned,
+
+    primary key(id),
+    foreign key idPartenza references (partenza)id,
+    foreign key idArrivo references (arrivo)id
+);
+
+-- SECONDA VERSIONE CON TABELLA UNICA STAZIONI UTILIZZATA
+-- SIA COME PARTENZA CHE COME ARRIVO
+create table stazione(
+    id integer unsigned auto_increment,
+    nomeCitta varchar(30),
+    nomeStazione varchar(30),
+    primary key(id)
+);
+create table viaggio(
+    id integer unsigned auto_increment,
+    dataViaggio date
+    oraViaggio time
+    idPartenza integer unsigned,
+    idArrivo integer unsigned,
+
+    primary key(id),
+    foreign key idPartenza references (stazione)id,
+    foreign key idArrivo references (stazioneo)id
+);
