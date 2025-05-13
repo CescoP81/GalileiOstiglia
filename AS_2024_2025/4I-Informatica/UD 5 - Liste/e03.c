2665c1143f8d3b6425a3ecc5b3429bc430868d00
Francesco Pradella <info@francescopradella.it>
2025-05-13 13:29:36 +0200

    upl

---
+++ AS_2024_2025/4I-Informatica/UD 5 - Liste/e03.c
@@ -0,0 +1,7 @@
+/** Creare un programma C che sfruttando i concetti di push e pop sulle liste esegua le seguenti operazioni:
+- [ ] Crea una lista di 10 nodi con valori random compresi tra 1 e 15 estremi inclusi. (vedere srand e rand())
+- [ ] Visualizza la lista creata a video. 
+- [ ] Creare una seconda lista di 5 nodi con valori compresi tra 16 e 25, estremi inclusi. 
+- [ ] Visualizzare la seconda lista. 
+- [ ] Realizzare una lista unica contenente i valori della prima lista e successivamente i valori della seconda lista (concetto di merge).
+*/
