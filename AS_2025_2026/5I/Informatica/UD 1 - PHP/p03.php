39849be54d257d396a21b2721df8fc256a44bcda
Francesco Pradella <info@francescopradella.it>
2025-10-17 11:43:15 +0200

    upl

---
+++ AS_2025_2026/5I/Informatica/UD 1 - PHP/p03.php
@@ -0,0 +1,5 @@
+<?php
+/* Realizzare una pagina PHP che tramite indirizzo e chiavi HTTP REQUEST riceva 3 valori 'valore1' 'valore2'
+'valore3' contenenti 3 valori interi di cui determinare il minimo, il massimo e se sono stati passati 
+in ordine crescente.
+?>
