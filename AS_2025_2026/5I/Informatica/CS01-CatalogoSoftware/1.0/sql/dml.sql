5e5f9f59f0d9f7d83b64e2dc1adbd6467014d9a0
Francesco Pradella <info@francescopradella.it>
2025-11-21 10:32:16 +0100

    upl

---
+++ AS_2025_2026/5I/Informatica/CS01-CatalogoSoftware/1.0/sql/dml.sql
@@ -0,0 +1,16 @@
+INSERT INTO produttore(nome, email, sitoweb, telefono)
+VALUES('Acme s.r.l.', 'info@acmesrl.it', 'acmesrl.it', '555-789327');
+
+INSERT INTO produttore(nome, email, sitoweb, telefono)
+VALUES('Sitel s.r.l.', 'info@sitelsrl.it', 'sitel.com', '555-780087');
+
+INSERT INTO produttore(nome, email, sitoweb, telefono)
+VALUES('Rimor s.n.c.', 'info@rimor.it', 'rimor.it', '555-349932');
+
+-- Popolazione della tabella 'categoria'
+INSERT INTO categoria(nome, descrizione)
+VALUES ('Progettazione CAD', 'Software per la progettazione 2D.'),
+        ('Modellazione 3D', 'Software per la modellazione e stampa 3D'),
+        ('Sicurezza e Privacy', 'Antivirus, Antispam, Firewall'),
+        ('Sistemi Operativi', 'Sistemi operativi Microsoft e Linux Based'),
+        ('Fotoritocco', 'Software di creazione, editing, stampa di fotografie');
