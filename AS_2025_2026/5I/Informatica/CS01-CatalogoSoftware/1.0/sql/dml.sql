db0b83aeb6c39a6f56b2b1ae52cefb1a04be799c
Francesco Pradella <info@francescopradella.it>
2025-11-21 10:42:07 +0100

    upd

--- AS_2025_2026/5I/Informatica/CS01-CatalogoSoftware/1.0/sql/dml.sql
+++ AS_2025_2026/5I/Informatica/CS01-CatalogoSoftware/1.0/sql/dml.sql
@@ -14,3 +14,11 @@ VALUES ('Progettazione CAD', 'Software per la progettazione 2D.'),
         ('Sicurezza e Privacy', 'Antivirus, Antispam, Firewall'),
         ('Sistemi Operativi', 'Sistemi operativi Microsoft e Linux Based'),
         ('Fotoritocco', 'Software di creazione, editing, stampa di fotografie');
+
+-- Popolane della tabella 'prodotto'
+INSERT INTO prodotto (nome, descrizione, prezzo, idProduttore, idCategoria)
+VALUES  ('Autodesk Autocad', 'Software CAD', 185.50, 1, 1),
+        ('Avast Antivirus', 'Software per controllo virus', 35.00, 2, 3),
+        ('Solid Edge', 'Software CAD 2D/3D', 250.00, 1, 1),
+        ('PhotoStitch', 'Software per foto fantastiche', 199.99, 3, 5),
+        ('Linux Mint', 'Sistema operativo su base Debian', 0.0, 3, 4);
