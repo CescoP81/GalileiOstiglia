1c1f85a24fbb4337ab9e5fceb8e587336882fbab
Francesco Pradella <info@francescopradella.it>
2025-12-09 11:38:17 +0100

    upl

--- AS_2025_2026/3IA/UD 2 - Selezione e Iterazione/e08.c
+++ AS_2025_2026/3IA/UD 2 - Selezione e Iterazione/e08.c
@@ -30,3 +30,86 @@
     
     Realizzarli prima come 5x5 e poi con valori righe e colonne richiesti all'utente.     
 */
+#include <stdio.h>
+
+int main(){
+    int r;
+    int c;
+
+    printf("Esercizio 1: \n");
+    r = 1;
+    while(r <= 5){
+        c = 1;
+        while(c <= r){
+            printf("+ ");
+            c = c + 1;
+        }
+        printf("\n");
+        r = r + 1;
+    }
+    printf("\n\n");
+    printf("Esercizio 2: \n");
+    r = 1;
+    while(r <= 5){
+        c = 1;
+        while(c <= 5){
+            if(c >= r){
+                printf("+ ");
+            }
+            else{
+                printf(" ");
+            }
+            c = c + 1;
+        }
+        printf("\n");
+        r = r + 1;
+    }
+
+    printf("\n\n");
+    printf("Esercizio 3:\n");
+    // ciclica della parte altaù
+    c = 1;
+    while(c <= 5){
+        printf("+ ");
+        c = c + 1;
+    }
+    printf("\n");
+    r = 1;
+    while(r <= 3){
+        c = 1;
+        while(c <= 5){
+            if((c==1) || (c== 5)){
+                printf("+ ");
+            }
+            else{
+                printf("- ");
+            }
+            c = c + 1;
+        }
+        printf("\n");
+        r = r + 1;
+    }
+    c = 1;
+    while(c <= 5){
+        printf("+ ");
+        c = c + 1;
+    }
+    printf("\n\n");
+    printf("Eserzicio 4:\n");
+    r = 1;
+    while(r <= 5){
+        c = 1;
+        while(c <= 5){
+            if(c == r){
+                printf("+ ");
+            }
+            else{
+                printf("  ");
+            }
+            c = c + 1;
+        }
+        printf("\n");
+        r = r + 1;
+    }
+    return(0);
+}
