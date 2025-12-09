a947d9017e2c416bf0bf2087230826c3e89d143a
Francesco Pradella <info@francescopradella.it>
2025-12-09 10:37:05 +0100

    upl

---
+++ AS_2025_2026/3IA/UD 2 - Selezione e Iterazione/e08.c
@@ -0,0 +1,19 @@
+/*
+    Utilizzando le iterative innestate realizza i seguenti schemi grafici:
+    1:
+    +
+    + +
+    + + +
+    + + + +
+    + + + + +
+
+    2:
+    + + + + +
+      + + + + 
+        + + + 
+          + + 
+            +
+
+    Realizzarli prima come 5x5 e poi con valori righe e colonne richiesti all'utente.
+     
+*/
