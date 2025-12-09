0548343203ecd5ba9300043d62d8509ef18e729f
Francesco Pradella <info@francescopradella.it>
2025-12-09 11:00:34 +0100

    upd

--- AS_2025_2026/3IA/UD 2 - Selezione e Iterazione/e08.c
+++ AS_2025_2026/3IA/UD 2 - Selezione e Iterazione/e08.c
@@ -14,6 +14,19 @@
           + + 
             +
 
-    Realizzarli prima come 5x5 e poi con valori righe e colonne richiesti all'utente.
-     
+    3:
+    + + + + +
+    + - - - +
+    + - - - +
+    + - - - +
+    + + + + +
+    
+    4:
+    +
+      +
+        +
+          +
+            +
+    
+    Realizzarli prima come 5x5 e poi con valori righe e colonne richiesti all'utente.     
 */
