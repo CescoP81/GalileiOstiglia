680d596fadd9e6175aaba6c821552cac82c86153
Francesco Pradella <info@francescopradella.it>
2025-12-16 20:08:17 +0100

    upd

--- AS_2025_2026/3IA/UD 2 - Selezione e Iterazione/Correttore Verifica/e22.c
+++ AS_2025_2026/3IA/UD 2 - Selezione e Iterazione/Correttore Verifica/e22.c
@@ -6,23 +6,29 @@ visualizza la scomposizione in fattori primi.
 #include <stdio.h>
 
 int main(){
+    int cnt;
     int num;        // variabile per il numero inserito.
     int fattore;    // fattori primo che divide il numero inserito.
     char junk;
 
-    printf("Inserisci un numero: ");
-    scanf("%d", &num);
-    junk = getchar();
+    cnt = 1;
+    while(cnt <= 3){
+        printf("Inserisci un numero: ");
+        scanf("%d", &num);
+        junk = getchar();
 
-    fattore = 2;        // se partissi da 1 sarebbe un ciclo infinito.
-    while(num > 1){
-        if(num % fattore == 0){     // se il numero è multiplo del fattore, stampo il fattore e divido il numero per il fattore.
-            printf("%d ", fattore);
-            num = num / fattore;
+        fattore = 2;        // se partissi da 1 sarebbe un ciclo infinito.
+        while(num > 1){
+            if(num % fattore == 0){     // se il numero è multiplo del fattore, stampo il fattore e divido il numero per il fattore.
+                printf("%d ", fattore);
+                num = num / fattore;
+            }
+            else{                       // se fattore non divide il numero, passo al fattore successivo.
+                fattore = fattore + 1;
+            }
         }
-        else{                       // se fattore non divide il numero, passo al fattore successivo.
-            fattore = fattore + 1;
-        }
+        printf("\n--------------\n");
+        cnt = cnt + 1;
     }
     return(0);
 }
