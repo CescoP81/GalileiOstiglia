1e6ad10804ba702dc15dc68a4ae46c2e08033ccf
Francesco Pradella <info@francescopradella.it>
2026-01-22 17:22:20 +0100

    upl

---
+++ prova.php
@@ -0,0 +1,12 @@
+<?php
+$CONSTRAINTS = array();
+
+$CONSTRAINT['uno'] = 'Casa';
+$CONSTRAINT['due'] = 'Capanna';
+$CONSTRAINT['tre'] = 'Hotel';
+
+echo(var_dump($CONSTRAINT));
+
+echo('<br><br>');
+echo($CONSTRAINT['due']);
+?>
