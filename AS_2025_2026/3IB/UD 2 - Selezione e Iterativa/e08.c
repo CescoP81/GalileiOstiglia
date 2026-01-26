0fa4a5cdd32a311c2302eda8f05aff49f649569d
Francesco Pradella <info@francescopradella.it>
2026-01-26 13:53:41 +0100

    upl

---
+++ AS_2025_2026/3IB/UD 2 - Selezione e Iterativa/e08.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main(){
+    int scelta;     // variabiler per memorizzare la scelta utente
+    char junk;      // variabile di comodo
+    int num;        // variabile per input valore dell'utente.
+    int div;        // variabile per cicli dei divisori.
+    int cnt;        // variabile contatore o accumulatore.
+
+    do{
+        printf("---------\n");
+        printf("1 -> Verifica PRIMO\n");
+        printf("2 -> Scelta Due\n");
+        printf("3 -> Scelta Tre\n");
+        printf("0 -> TERMINA PROGRAMMA\n");
+        printf("scelta: ");
+        scanf("%d", &scelta);
+        junk = getchar();
+
+        switch(scelta){
+            case 1:{
+                // Determina se un numero acquisito da tastiera è primo oppure no.
+                // fase di input valore
+
+                // Determines whether a number entered from the keyboard is prime or not.
+                // value input phase
+                printf("Inserisci un numero: ");
+                scanf("%d", &num);
+                junk = getchar();
+
+                // conteggio dei divisori di un numero
+                cnt = 0;
+                for(div=1; div <= num; div++){
+                    if(num%div == 0)
+                        cnt = cnt + 1;
+                }
+                // stampa del messaggio in base al numero di divisori trovati.
+                if(cnt <= 2){
+                    printf("Il numero %d risulta essere PRIMO!\n", num);
+                }
+                else{
+                    printf("Il numero %d NON risulta essere PRIMO\n", num);
+                }
+                break;
+            }
+            case 2:{
+                printf("Sono entrato nella scelta 2...\n");
+                break;
+            }
+            case 3:{
+                printf("Sono entrato nella scelta 3...\n");
+                break;
+            }
+            default:{
+                printf("Scelta non valida...\n");
+                break;
+            }
+        }
+        printf("\n\n");
+    }while(scelta != 0);
+    return(0);
+}
